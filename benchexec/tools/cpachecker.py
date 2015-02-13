74f9b479df136ebd698c9768ebed1d22289665de
Philipp Wendler <uni@philippwendler.de>
2015-02-13 13:07:13 +0100

    Update copyright statement.

--- benchexec/tools/cpachecker.py
+++ benchexec/tools/cpachecker.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-
 """
-CPAchecker is a tool for configurable software verification.
-This file is part of CPAchecker.
+BenchExec is a framework for reliable benchmarking.
+This file is part of BenchExec.
 
-Copyright (C) 2007-2014  Dirk Beyer
+Copyright (C) 2007-2015  Dirk Beyer
 All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,10 +17,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-
-CPAchecker web page:
-  http://cpachecker.sosy-lab.org
 """
 
 # prepare for Python 3
