9596589c4c6f618817308094852e86c000f5437c
Philipp Wendler <uni@philippwendler.de>
2015-06-22 15:13:02 +0200

    Prepare version number for next development cycle.

--- benchexec/__init__.py
+++ benchexec/__init__.py
@@ -60,7 +60,7 @@ Variables ending with "file" contain filenames.
 Variables ending with "tag" contain references to XML tag objects created by the XML parser.
 """
 
-__version__ = '0.5'
+__version__ = '0.6-dev'
 
 class BenchExec(object):
     """
