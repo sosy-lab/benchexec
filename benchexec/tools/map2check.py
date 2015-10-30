ba0a4a3e2fbc1021ee664c119003b7e3c5bd43d9
Herbert <herberthb12@gmail.com>
2015-10-30 05:43:46 -0400

    Update map2check.py
    Added license to the file and in the version function, it is adopted the function self._version_from_tool to make the code easier.

--- benchexec/tools/map2check.py
+++ benchexec/tools/map2check.py
@@ -1,3 +1,22 @@
+"""
+BenchExec is a framework for reliable benchmarking.
+This file is part of BenchExec.
+
+Copyright (C) 2007-2015  Dirk Beyer
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import subprocess
 import os
 import re
@@ -14,36 +33,29 @@ class Tool(benchexec.tools.template.BaseTool):
         #Relative path to map2check wrapper
         return Util.find_executable('wrapper_script_map2check.sh')
 
-
     def program_files(self, executable):
         executableDir = os.path.dirname(executable)
         return [executableDir]
-
 
     def working_directory(self, executable):
         executableDir = os.path.dirname(executable)
         return executableDir
 
-
     def environment(self, executable):
         return {"additionalEnv" : {'PATH' :  ':.'}}
 
-
     def version(self, executable):
-        workingDir = self.working_directory(executable)
-        return str(subprocess.Popen([workingDir + '/map2check.py', '--version'],
-                                stdout=subprocess.PIPE).communicate()[0]).strip()
-
+        return self._version_from_tool(executable)
 
     def name(self):
         return 'Map2Check'
-
 
     def cmdline(self, executable, options, sourcefiles, propertyfile, rlimits):
         assert len(sourcefiles) == 1, "only one sourcefile supported"
         sourcefile = sourcefiles[0]
         workingDir = self.working_directory(executable)        
         return [os.path.relpath(executable, start=workingDir)] + options + ['-c', propertyfile, os.path.relpath(sourcefile, start=workingDir)]
+
 
 
     def determine_result(self, returncode, returnsignal, output, isTimeout):
