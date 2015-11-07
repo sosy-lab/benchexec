8a2c1739f09b601fbcfa87ecbb90115c1d6186d4
Williame Rocha <williame.rocha10@gmail.com>
2015-11-07 17:07:12 -0400

    Update depthk.py
    Follows new version.

--- benchexec/tools/depthk.py
+++ benchexec/tools/depthk.py
@@ -1,3 +1,18 @@
+"""
+BenchExec is a framework for reliable benchmarking.
+This file is part of BenchExec.
+Copyright (C) 2007-2015  Dirk Beyer
+All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import subprocess
 import os
@@ -11,10 +26,8 @@ class Tool(benchexec.tools.template.BaseTool):
 
     """
     This class serves as tool adaptor for DepthK (www.esbmc.org)
-    Autor: Williame Rocha - williame.rocha10@gmail.com
-    	   Herbert Rocha - herberthb12@gmail.com
+    Autor: Williame Rocha - williame.rocha10@gmail.com - Federal University of Amazonas, Brazil.
     """
-
     def executable(self):
 
         # Relative path to depthk wrapper
@@ -34,8 +47,11 @@ class Tool(benchexec.tools.template.BaseTool):
 
     def version(self, executable):
         workingDir = self.working_directory(executable)
-        return subprocess.Popen([workingDir + '/depthk.py', '--version'],
-                                stdout=subprocess.PIPE).communicate()[0].strip()
+
+        version = subprocess.Popen([workingDir + '/depthk.py',
+                                   '--version'],
+                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.readline().decode()
+        return version
 
     def name(self):
         return 'DepthK'
@@ -48,11 +64,13 @@ class Tool(benchexec.tools.template.BaseTool):
         propertyfile,
         rlimits,
         ):
+
         assert len(tasks) == 1, 'only one sourcefile supported'
         sourcefile = tasks[0]
         workingDir = self.working_directory(executable)
         return [os.path.relpath(executable, start=workingDir)] \
-            + options + ['-c', propertyfile, os.path.relpath(sourcefile, start=workingDir)]
+            + options + ['-c', propertyfile,
+                         os.path.relpath(sourcefile, start=workingDir)]
 
     def determine_result(
         self,
@@ -88,6 +106,6 @@ class Tool(benchexec.tools.template.BaseTool):
 
         return status
 
-    
+
 
 			
