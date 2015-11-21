62c6c382d5af5d027b5ec8d50a81d48c5b27b533
Philipp Wendler <uni@philippwendler.de>
2015-11-21 16:55:10 +0100

    Refactoring: move utility methods out of table-generator module

--- benchexec/tablegenerator/__init__.py
+++ benchexec/tablegenerator/__init__.py
@@ -26,7 +26,6 @@ import collections
 import os.path
 import glob
 import itertools
-import json
 import logging
 import argparse
 import re
@@ -35,7 +34,7 @@ import sys
 import time
 import tempita
 
-from decimal import *
+from decimal import Decimal, InvalidOperation
 
 # Process pool for parallel work.
 # Some of our loops are CPU-bound (e.g., statistics calculations), thus we use
@@ -45,6 +44,7 @@ parallel = None
 
 from .. import __version__
 import benchexec.result as result
+from . import util as Util
 
 NAME_START = "results" # first part of filename of table
 
@@ -57,159 +57,6 @@ TEMPLATE_FILE_NAME = os.path.join(os.path.dirname(__file__), 'template.{format}'
 TEMPLATE_FORMATS = ['html', 'csv']
 TEMPLATE_ENCODING = 'UTF-8'
 
-DEFAULT_TIME_PRECISION = 3
-
-class Util:
-    """
-    This Class contains some useful functions for Strings, Files and Lists.
-    """
-
-    @staticmethod
-    def get_file_list(shortFile):
-        """
-        The function get_file_list expands a short filename to a sorted list
-        of filenames. The short filename can contain variables and wildcards.
-        """
-
-        # expand tilde and variables
-        expandedFile = os.path.expandvars(os.path.expanduser(shortFile))
-
-        # expand wildcards
-        fileList = glob.glob(expandedFile)
-
-        # sort alphabetical,
-        # if list is emtpy, sorting returns None, so better do not sort
-        if len(fileList) != 0:
-            fileList.sort()
-        else:
-            logging.warning("No file matches '%s'.", shortFile)
-
-        return fileList
-
-
-    @staticmethod
-    def extend_file_list(filelist):
-        '''
-        This function takes a list of files, expands wildcards
-        and returns a new list of files.
-        '''
-        return [file for wildcardFile in filelist for file in Util.get_file_list(wildcardFile)]
-
-
-    @staticmethod
-    def split_number_and_unit(s):
-        """
-        Split a string into two parts: a number prefix and an arbitrary suffix.
-        Splitting is done from the end, so the split is where the last digit
-        in the string is (that means the prefix may include non-digit characters,
-        if they are followed by at least one digit).
-        """
-        if not s:
-            return (s, '')
-        pos = len(s)
-        while pos and not s[pos-1].isdigit():
-            pos -= 1
-        return (s[:pos], s[pos:])
-
-    @staticmethod
-    def remove_unit(s):
-        """
-        Remove a unit from a number string, or return the full string if it is not a number.
-        """
-        (prefix, suffix) = Util.split_number_and_unit(s)
-        return suffix if prefix == '' else prefix
-
-    @staticmethod
-    def format_number(s, number_of_digits):
-        """
-        If the value is a number (or number plus one char),
-        this function returns a string-representation of the number
-        with a number of digits after the decimal separator.
-        If the number has more digits, it is rounded, else zeros are added.
-
-        If the value is no number, it is returned unchanged.
-        """
-        # if the number ends with "s" or another unit, remove it
-        value, suffix = Util.split_number_and_unit((str(s) or '').strip())
-        try:
-            floatValue = float(value)
-            return "{value:.{width}f}{suffix}".format(width=number_of_digits, value=floatValue, suffix=suffix)
-        except ValueError: # if value is no float, don't format it
-            return s
-
-
-    @staticmethod
-    def format_value(value, column):
-        """
-        Format a value nicely for human-readable output (including rounding).
-        """
-        if not value:
-            return '-'
-
-        number_of_digits = column.number_of_digits
-        if number_of_digits is None and column.title.lower().endswith('time'):
-            number_of_digits = DEFAULT_TIME_PRECISION
-
-        if number_of_digits is None:
-            return value
-        return Util.format_number(value, number_of_digits)
-
-
-    @staticmethod
-    def to_decimal(s):
-        # remove whitespaces and trailing units (e.g., in '1.23s')
-        s, _ = Util.split_number_and_unit((s or '').strip())
-        return Decimal(s) if s else None
-
-
-    @staticmethod
-    def collapse_equal_values(values, counts):
-        """
-        Take a tuple (values, counts), remove consecutive values and increment their count instead.
-        """
-        assert len(values) == len(counts)
-        previousValue = values[0]
-        previousCount = 0
-
-        for value, count in zip(values, counts):
-            if value != previousValue:
-                yield (previousValue, previousCount)
-                previousCount = 0
-                previousValue = value
-            previousCount += count
-
-        yield (previousValue, previousCount)
-
-    @staticmethod
-    def get_column_value(sourcefileTag, columnTitle, default=None):
-        for column in sourcefileTag.findall('column'):
-            if column.get('title') == columnTitle:
-                    return column.get('value')
-        return default
-
-    @staticmethod
-    def flatten(list_):
-        return [value for sublist in list_ for value in sublist]
-
-    @staticmethod
-    def json(obj):
-        return tempita.html(json.dumps(obj, sort_keys=True))
-
-    @staticmethod
-    def prettylist(list_):
-        if not list_:
-            return ''
-
-        # Filter out duplicate values while keeping order
-        values = set()
-        uniqueList = []
-        for entry in list_:
-            if not entry in values:
-                values.add(entry)
-                uniqueList.append(entry)
-
-        return uniqueList[0] if len(uniqueList) == 1 \
-            else '[' + '; '.join(uniqueList) + ']'
 
 def parse_table_definition_file(file, options):
     '''
@@ -1108,7 +955,7 @@ def create_tables(name, runSetResults, rows, rowsDiff, outputPath, outputFilePat
     count_id_columns = relevant_id_columns.count(True)
 
     templateNamespace={'flatten': Util.flatten,
-                       'json': Util.json,
+                       'json': Util.to_json,
                        'relpath': os.path.relpath,
                        'format_value': Util.format_value,
                        'split_number_and_unit': Util.split_number_and_unit,
