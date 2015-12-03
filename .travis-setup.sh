7ff49041effc1d1347cdba52c724eba1bfd0e973
Philipp Wendler <uni@philippwendler.de>
2015-12-03 14:47:10 +0100

    Add setup for cgroups and sudo to travis build such that we can run the respective tests on TravisCI.

---
+++ .travis-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+echo ------------------------
+echo "Existing mount points:"
+mount
+echo ------------------------
+echo "Existing users:"
+getent passwd
+echo ------------------------
+
+PRIMARY_USER="$1"
+SECONDARY_USER="$2"
+
+# Set up cgroups
+for i in cpuacct cpuset freezer memory; do
+  if [ ! -d "/sys/fs/cgroup/$i" ]; then
+    mkdir /sys/fs/cgroup/$i
+    mount cgroup-$i /sys/fs/cgroup/$i -t cgroup -o $i
+  fi
+  chgrp "$(id -g "$PRIMARY_USER")" /sys/fs/cgroup/$i
+  chmod g+rwx /sys/fs/cgroup/$i
+done
+
+# Set up sudo
+adduser --disabled-login --gecos "" "$SECONDARY_USER"
+echo "$PRIMARY_USER ALL=($SECONDARY_USER) NOPASSWD: ALL" >> /etc/sudoers
