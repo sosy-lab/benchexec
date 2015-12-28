40178c27514df795d86bbbed0dec246c3bf40b14
Dirk Beyer <dbeyer>
2015-12-28 12:16:09 +0100

    Fixed tool-info module for Predator

--- benchexec/tools/predatorhp.py
+++ benchexec/tools/predatorhp.py
@@ -37,21 +37,10 @@ class Tool(benchexec.tools.template.BaseTool):
                   "predator-bfs",
                   "predator-dfs",
                   "predatorHP.py",
-                  "predator-repo"
                   ]
 
     def executable(self):
-        executable = util.find_executable('predatorHP.py')
-        executableDir = os.path.dirname(executable)
-        if not os.path.isfile(os.path.join(executableDir, "predator-build-ok")):
-            self._buildPredatorHp(executableDir)
-        return executable
-
-    def _buildPredatorHp(self, executableDir):
-        proc = subprocess.Popen([os.path.join(executableDir, 'build-all.sh')], cwd=executableDir)
-        proc.communicate()
-        if proc.returncode:
-            sys.exit('Failed to build Predator-HP, please fix the build first.')
+        return util.find_executable('predatorHP.py')
 
     def name(self):
         return 'Predator-HP'
@@ -78,13 +67,3 @@ class Tool(benchexec.tools.template.BaseTool):
         if (status == "UNKNOWN" and isTimeout):
             status = "TIMEOUT"
         return status
-
-    def program_files(self, executable):
-        """
-        Returns a list of files or directories that are necessary to run the tool.
-        """
-        return [executable]
-
-
-    def working_directory(self, executable):
-        return os.path.dirname(executable)
