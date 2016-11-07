6afa17bcfd2ed579d4ecdf7b306f4292988ae3df
anushrijana <anushri.jana@tcs.com>
2016-11-07 19:26:04 +0530

    Add files via upload

--- benchexec/tools/veriabs.py
+++ benchexec/tools/veriabs.py
@@ -8,36 +8,27 @@ import benchexec.result as result
 
 class Tool(benchexec.tools.template.BaseTool):
     """
-	veriabs for SV-COMP 2017
+	VeriAbs
     """
+
     def executable(self):
         return util.find_executable('veriabs')
 
 
-    def version(self, executable):
-        return " Version 0.001 Alpha"
-
-
     def name(self):
-        return 'VeriAbs SV-COMP 2017'
+        return 'VeriAbs'
 
 
     def cmdline(self, executable, options, tasks, propertyfile, rlimits):
         if propertyfile:
             options += ['--property-file', propertyfile]
-        self.options = options
         return [executable] + options + tasks
-
-
 
 
     def determine_result(self, returncode, returnsignal, output, isTimeout):
         lines = " ".join(output[-10:])
-        print(lines)
-
         if isTimeout:
             return 'TIMEOUT' 
-
         if "INVALID-POINTER" in lines or "DYNAMIC_OBJECT" in lines or "dereference failure" in lines:
             return result.RESULT_FALSE_DEREF		
         elif "SUCCESS" in lines:
@@ -52,4 +43,3 @@ class Tool(benchexec.tools.template.BaseTool):
             return result.RESULT_UNKNOWN
         else:
             return result.RESULT_ERROR
-
