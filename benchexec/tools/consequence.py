9300124c809088914be4cb6db2459d6fdcbdcc00
anushrijana <anushri.jana@tcs.com>
2016-11-07 19:08:08 +0530

    Add files via upload

--- benchexec/tools/consequence.py
+++ benchexec/tools/consequence.py
@@ -8,26 +8,29 @@ import benchexec.result as result
 
 class Tool(benchexec.tools.template.BaseTool):
     """
-	veriabs for SV-COMP 2017
+	ConSequence for SV-COMP 2017
     """
+    REQUIRED_PATHS = [
+                  "consequence.pl"
+                  ]
     def executable(self):
-        return util.find_executable('veriabs')
+        return util.find_executable('consequence.pl')
 
 
     def version(self, executable):
-        return " Version 0.001 Alpha"
+        return "Version 1.0"
 
 
     def name(self):
-        return 'VeriAbs SV-COMP 2017'
+        return 'ConSequence SV-COMP 2017'
 
-
+    """
     def cmdline(self, executable, options, tasks, propertyfile, rlimits):
         if propertyfile:
             options += ['--property-file', propertyfile]
         self.options = options
         return [executable] + options + tasks
-
+    """
 
 
 
@@ -38,17 +41,11 @@ class Tool(benchexec.tools.template.BaseTool):
         if isTimeout:
             return 'TIMEOUT' 
 
-        if "INVALID-POINTER" in lines or "DYNAMIC_OBJECT" in lines or "dereference failure" in lines:
-            return result.RESULT_FALSE_DEREF		
-        elif "SUCCESS" in lines:
+        if "success" in lines:
             return result.RESULT_TRUE_PROP
-        elif "FAILED" in lines:
+        elif "failed" in lines:
             return result.RESULT_FALSE_REACH
-        elif "OUTOFMEMORY" in lines:
-            return 'OUT OF MEMORY'
-        elif "TIMEOUT" in lines:
-            return "TIMEOUT"
-        elif "NOT SUPPORTED" in lines or "UNKNOWN" in lines:
+        elif "unknown" in lines:
             return result.RESULT_UNKNOWN
         else:
             return result.RESULT_ERROR
