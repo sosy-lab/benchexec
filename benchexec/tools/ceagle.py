aed87bda32959e0245157050681ebca1f144775b
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2016-11-27 17:36:51 +0100

    Remove function that is not necessary anymore

--- benchexec/tools/ceagle.py
+++ benchexec/tools/ceagle.py
@@ -64,6 +64,3 @@ class Tool(benchexec.tools.template.BaseTool):
             status = result.RESULT_UNKNOWN
 
         return status
-
-    def environment(self, executable):
-        return {"additionalEnv": {"PATH": ":."}}
