bdbb78dbe366f77ef794e1d6c6074c14eed03648
Williame Rocha <williame.rocha10@gmail.com>
2016-12-18 01:10:47 -0400

    improvements on version method

--- benchexec/tools/depthk.py
+++ benchexec/tools/depthk.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
+
 """
 BenchExec is a framework for reliable benchmarking.
 This file is part of BenchExec.
-
 Copyright (C) 2007-2016  Dirk Beyer
 All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,25 +31,22 @@ class Tool(benchexec.tools.template.BaseTool):
     Autor: Williame Rocha - williame.rocha10@gmail.com - Federal University of Amazonas, Brazil.
     """
 
-    REQUIRED_PATHS = [
-        'depthk.py',
-        'depthk-wrapper.sh',
-        'esbmc',
-        '__init__.py',
-        'modules',
-        ]
+    REQUIRED_PATHS = ['depthk.py', 'depthk-wrapper.sh', 'esbmc',
+                      '__init__.py', 'modules']
 
     def executable(self):
-        
-        return Util.find_executable('depthk-wrapper.sh'
-                                    )
+
+        return Util.find_executable('depthk-wrapper.sh')
 
     def working_directory(self, executable):
         executableDir = os.path.dirname(executable)
         return executableDir
 
     def version(self, executable):
-        return self._version_from_tool(executable, '-v')
+        version = subprocess.Popen([executable, '-v'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT).stdout.readline().decode()
+        return version
 
     def name(self):
         return 'DepthK'
@@ -103,7 +97,7 @@ class Tool(benchexec.tools.template.BaseTool):
         return status
 
     def get_value_from_output(self, lines, identifier):
-     
+
         for line in lines:
             if identifier == 'k' and line.startswith('Bound k:'):
                 matchbound = re.search(r'Bound k:(.*)', line)
@@ -113,3 +107,4 @@ class Tool(benchexec.tools.template.BaseTool):
                 return matchstep.group(1).strip()
 
         return '-'
+
