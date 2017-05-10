e6e7d805d2a33839c663e19e8ec95e348dd52d3f
Leo Strakosch <lembergerth@gmail.com>
2017-05-10 15:21:01 +0200

    Replicate directory structure in temporary directory for integration tests.
    This makes the location of the temporary directory independent
    of the test directory.

--- benchexec/test_benchexec_integration.py
+++ benchexec/test_benchexec_integration.py
@@ -35,7 +35,7 @@ sys.dont_write_bytecode = True # prevent creation of .pyc files
 here = os.path.dirname(__file__)
 base_dir = os.path.join(here, '..')
 bin_dir = os.path.join(base_dir, 'bin')
-tmp_dir = os.path.join(here, 'test_integration')
+benchmarks_dir = os.path.join(base_dir, 'doc')
 benchexec = os.path.join(bin_dir, 'benchexec')
 result_dtd = os.path.join(base_dir, 'doc', 'result.dtd')
 result_dtd_public_id = '+//IDN sosy-lab.org//DTD BenchExec result 1.9//EN'
@@ -52,8 +52,27 @@ class BenchExecIntegrationTests(unittest.TestCase):
         cls.longMessage = True
         cls.maxDiff = None
 
+    def _build_tmp_dir(self):
+        """
+        Initializes the temporary directory structure for testing.
+        Current structure:
+            $TMP_DIR$
+                |-- doc                                # contains benchmark definitions
+                |-- benchexec/test_integration/actual  # output directory for benchexec runs
+        """
+        tmp_dir = tempfile.mkdtemp(prefix="BenchExec.benchexec.integration_test")
+        relative_benchmark_dir = os.path.relpath(benchmarks_dir, base_dir)
+        tmp_benchmarks_dir = os.path.join(tmp_dir, relative_benchmark_dir)
+        shutil.copytree(benchmarks_dir, tmp_benchmarks_dir)
+        output_dir = os.path.join(tmp_dir, 'benchexec', 'test_integration', 'actual')
+        os.makedirs(output_dir)
+        self.tmp = tmp_dir
+        self.benchmarks_dir = tmp_benchmarks_dir
+        self.output_dir = output_dir
+        self.benchmark_test_file = os.path.join(self.tmp, os.path.relpath(benchmark_test_file, base_dir))
+
     def setUp(self):
-        self.tmp = tempfile.mkdtemp(prefix="BenchExec.benchexec.integration_test", dir=tmp_dir)
+        self._build_tmp_dir()
 
     def tearDown(self):
         shutil.rmtree(self.tmp)
@@ -71,15 +90,17 @@ class BenchExecIntegrationTests(unittest.TestCase):
                                                  tasks=benchmark_test_tasks,
                                                  rundefs=benchmark_test_rundefs,
                                                  test_name=benchmark_test_name,
-                                                 test_file=benchmark_test_file,
+                                                 test_file=None,
                                                  compress=False):
+        if not test_file:  # Assign default test file
+            test_file = self.benchmark_test_file
         self.run_cmd(*[benchexec, test_file,
-                       '--outputpath', self.tmp,
+                       '--outputpath', self.output_dir,
                        '--startTime', '2015-01-01 00:00',
                        ]
                        + ([] if compress else ['--no-compress-results'])
                        + list(args))
-        generated_files = set(os.listdir(self.tmp))
+        generated_files = set(os.listdir(self.output_dir))
 
         xml_suffix = '.xml.bz2' if compress else '.xml'
 
@@ -110,7 +131,7 @@ class BenchExecIntegrationTests(unittest.TestCase):
         # TODO find way to compare expected output to generated output
 
         if compress:
-            with zipfile.ZipFile(os.path.join(self.tmp, basename + "logfiles.zip")) as log_zip:
+            with zipfile.ZipFile(os.path.join(self.output_dir, basename + "logfiles.zip")) as log_zip:
                 self.assertIsNone(log_zip.testzip(), "Logfiles zip archive is broken")
                 for file in log_zip.namelist():
                     self.assertTrue(file.startswith(basename + "logfiles" + os.sep),
@@ -119,7 +140,7 @@ class BenchExecIntegrationTests(unittest.TestCase):
             for file in generated_files:
                 if file.endswith(".bz2"):
                     # try to decompress and read to see if there are any errors with it
-                    with bz2.BZ2File(os.path.join(self.tmp, file)) as bz2file:
+                    with bz2.BZ2File(os.path.join(self.output_dir, file)) as bz2file:
                         bz2file.read()
 
     def _assertEqualXmlTree(self, actual_tree, expected_tree):
@@ -232,14 +253,14 @@ class BenchExecIntegrationTests(unittest.TestCase):
             raise e
 
     def test_validate_result_xml(self):
-        self.run_cmd(benchexec, benchmark_test_file,
-                     '--outputpath', self.tmp,
+        self.run_cmd(benchexec, self.benchmark_test_file,
+                     '--outputpath', self.output_dir,
                      '--startTime', '2015-01-01 00:00',
                      '--no-compress-results',
                      )
         basename = 'benchmark-example-rand.2015-01-01_0000.'
         xml_files = ['results.xml'] + ['results.'+files+'.xml' for files in benchmark_test_tasks]
-        xml_files = map(lambda x : os.path.join(self.tmp, basename + x), xml_files)
+        xml_files = map(lambda x : os.path.join(self.output_dir, basename + x), xml_files)
 
         # setup parser with DTD validation
         from lxml import etree
@@ -256,12 +277,12 @@ class BenchExecIntegrationTests(unittest.TestCase):
 
     def test_run_results_information(self):
         expected_xml = os.path.join(here, 'test_integration/expected/benchmark-example-true.2015-01-01_0000.results.no options.xml')
-        benchmark_xml = os.path.join(base_dir, 'doc', 'benchmark-example-true.xml')
+        benchmark_xml = os.path.join(self.benchmarks_dir, 'benchmark-example-true.xml')
         self.run_cmd(benchexec, benchmark_xml,
-                     '--outputpath', self.tmp,
+                     '--outputpath', self.output_dir,
                      '--startTime', '2015-01-01 00:00',
                      '--no-compress-results',
                      '--rundefinition', 'no options')
-        actual_xml = os.path.join(self.tmp, 'benchmark-example-true.2015-01-01_0000.results.no options.xml')
+        actual_xml = os.path.join(self.output_dir, 'benchmark-example-true.2015-01-01_0000.results.no options.xml')
 
         self.assertSameRunResults(actual_xml, expected_xml)
