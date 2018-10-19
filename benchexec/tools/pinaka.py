64bd681052ad602fcbbe27414be9fb5f0439891a
EtiChaudhary <cs17mtech11029@iith.ac.in>
2018-10-20 03:17:02 +0530

    Codacy Issue: Trailing Spaces corrected

--- benchexec/tools/pinaka.py
+++ benchexec/tools/pinaka.py
@@ -3,14 +3,14 @@ import benchexec.tools.template
 import benchexec.result as result
 
 class Tool(benchexec.tools.template.BaseTool):
-    
+
     REQUIRED_PATHS = [
                   "pinaka-wrapper.sh",
                   "pinaka"
                   ]
 
     def executable(self):
-        return util.find_executable('pinaka-wrapper.sh')	
+        return util.find_executable('pinaka-wrapper.sh')
 
     def name(self):
         return "Pinaka"
@@ -26,6 +26,6 @@ class Tool(benchexec.tools.template.BaseTool):
             else:
                 status = result.RESULT_UNKNOWN
         else:
-            status = result.RESULT_ERROR        
-        
+            status = result.RESULT_ERROR
+
         return status
