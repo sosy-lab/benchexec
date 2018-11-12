9083bf5c26843e68be35e79b00248a9bd5fd20ad
ylz <yinliangze@163.com>
2018-11-13 00:47:28 +0800

    add version number for yogar-cbmc-parallel

--- benchexec/tools/yogar-cbmc-parallel.py
+++ benchexec/tools/yogar-cbmc-parallel.py
@@ -19,11 +19,18 @@ yogar_cbmc = __import__("benchexec.tools.yogar-cbmc", fromlist=["Tool"])
 
 class Tool(yogar_cbmc.Tool):
 
+    REQUIRED_PATHS = [
+                  "yogar-cbmc"
+                  ]
+        
     def executable(self):
         return util.find_executable('yogar-cbmc-parallel')
 
     def name(self):
         return 'Yogar-CBMC-Parallel'
+        
+    def version(self, executable):
+		return self._version_from_tool(executable)
 
     def cmdline(self, executable, options, tasks, propertyfile, rlimits):
         return [executable] + options + tasks
