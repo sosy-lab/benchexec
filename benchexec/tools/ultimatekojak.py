e051b1ae41615c1fcc3ba3a751c8fd92e8b134e7
danieldietsch <daniel.dietsch@gmail.com>
2018-11-14 10:49:27 +0100

    updated description of Ultimate tools

--- benchexec/tools/ultimatekojak.py
+++ benchexec/tools/ultimatekojak.py
@@ -2,7 +2,7 @@
 BenchExec is a framework for reliable benchmarking.
 This file is part of BenchExec.
 
-Copyright (C) 2015  Daniel Dietsch
+Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
 All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,37 +20,49 @@ limitations under the License.
 
 from . import ultimate
 
+
 class Tool(ultimate.UltimateTool):
+    """
+    This is the tool info module for ULTIMATE Kojak.
+
+    You can download the latest release from GitHub or build the latest development snapshot by following the
+    instructions at https://github.com/ultimate-pa/ultimate/wiki/Usage
+
+    Please report any issues to our issue tracker at https://github.com/ultimate-pa/ultimate/issues
+
+    Latest release: https://github.com/ultimate-pa/ultimate/releases/latest
+    Git repository: https://github.com/ultimate-pa/ultimate.git
+    """
 
     REQUIRED_PATHS_SVCOMP17 = [
-                  "artifacts.xml",
-                  "configuration",
-                  "cvc4",
-                  "features",
-                  "KojakMemDerefMemtrack.xml",
-                  "KojakReach.xml",
-                  "LICENSE",
-                  "LICENSE.GPL",
-                  "LICENSE.GPL.LESSER",
-                  "p2",
-                  "plugins",
-                  "README",
-                  "svcomp-DerefFreeMemtrack-32bit-Kojak_Bitvector.epf",
-                  "svcomp-DerefFreeMemtrack-32bit-Kojak_Default.epf",
-                  "svcomp-DerefFreeMemtrack-64bit-Kojak_Bitvector.epf",
-                  "svcomp-DerefFreeMemtrack-64bit-Kojak_Default.epf",
-                  "svcomp-Overflow-32bit-Kojak_Default.epf",
-                  "svcomp-Overflow-64bit-Kojak_Default.epf",
-                  "svcomp-Reach-32bit-Kojak_Bitvector.epf",
-                  "svcomp-Reach-32bit-Kojak_Default.epf",
-                  "svcomp-Reach-64bit-Kojak_Bitvector.epf",
-                  "svcomp-Reach-64bit-Kojak_Default.epf",
-                  "Ultimate",
-                  "Ultimate.ini",
-                  "Ultimate.py",
-                  "z3",
-                  "mathsat"
-                  ]
+        "artifacts.xml",
+        "configuration",
+        "cvc4",
+        "features",
+        "KojakMemDerefMemtrack.xml",
+        "KojakReach.xml",
+        "LICENSE",
+        "LICENSE.GPL",
+        "LICENSE.GPL.LESSER",
+        "p2",
+        "plugins",
+        "README",
+        "svcomp-DerefFreeMemtrack-32bit-Kojak_Bitvector.epf",
+        "svcomp-DerefFreeMemtrack-32bit-Kojak_Default.epf",
+        "svcomp-DerefFreeMemtrack-64bit-Kojak_Bitvector.epf",
+        "svcomp-DerefFreeMemtrack-64bit-Kojak_Default.epf",
+        "svcomp-Overflow-32bit-Kojak_Default.epf",
+        "svcomp-Overflow-64bit-Kojak_Default.epf",
+        "svcomp-Reach-32bit-Kojak_Bitvector.epf",
+        "svcomp-Reach-32bit-Kojak_Default.epf",
+        "svcomp-Reach-64bit-Kojak_Bitvector.epf",
+        "svcomp-Reach-64bit-Kojak_Default.epf",
+        "Ultimate",
+        "Ultimate.ini",
+        "Ultimate.py",
+        "z3",
+        "mathsat"
+    ]
 
     def name(self):
         return 'ULTIMATE Kojak'
