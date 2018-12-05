59f26ed47828ad3166217502273b48420c95fbb9
Thomas Lemberger <lembergerth@gmail.com>
2018-12-05 19:28:19 +0100

    Tool-info module for TBF handles TBF verdict 'done' as 'done'

--- benchexec/tools/tbf.py
+++ benchexec/tools/tbf.py
@@ -71,6 +71,8 @@ class Tool(benchexec.tools.template.BaseTool):
                 return result.RESULT_FALSE_REACH
             elif line.startswith('TBF') and 'TRUE' in line:
                 return result.RESULT_TRUE_PROP
+            elif line.startswith('TBF') and 'DONE' in line:
+                return result.RESULT_DONE
         return result.RESULT_UNKNOWN
 
     def get_value_from_output(self, lines, identifier):
