05b8ba42ab866aefe7dbb1e7af0c2f19ff2ba3ba
Thomas Lemberger <lembergerth@gmail.com>
2019-01-16 17:05:22 +0100

    Remove unnecessary else-branch

--- benchexec/tablegenerator/__init__.py
+++ benchexec/tablegenerator/__init__.py
@@ -700,12 +700,6 @@ class RunResult(object):
                     # If no score column exists in the xml, take the internally computed score,
                     # if available
                     value = str(score) if score is not None else None
-                else:
-                    try:
-                        score = float(value)
-                    except ValueError:
-                        logging.error("Column 'score' is selected, but contains non-number value: %s", value)
-                        exit(1)
             values.append(value)
 
         sourcefiles = sourcefileTag.get('files')
