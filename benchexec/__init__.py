9639db42cd054269c5f3c556ff35be216eb91786
Philipp Wendler <uni@philippwendler.de>
2019-06-13 14:46:28 +0200

    Prepare version number for next development cycle.

--- benchexec/__init__.py
+++ benchexec/__init__.py
@@ -49,7 +49,7 @@ Variables ending with "tag" contain references to XML tag objects created by the
 # prepare for Python 3
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "1.20"
+__version__ = '1.21-dev'
 
 
 class BenchExecException(Exception):
