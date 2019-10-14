3a544a95eb0bb54c218e6eeefc1182dcc7c2f5bd
Hernan Ponce de Leon <ponce@fortiss.org>
2019-10-14 15:12:38 +0200

    dartagnan integration

---
+++ benchexec/tools/dartagnan.py
@@ -0,0 +1,47 @@
+"""
+BenchExec is a framework for reliable benchmarking.
+This file is part of BenchExec.
+Copyright (C) 2007-2015  Dirk Beyer
+All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import logging
+import xml.etree.ElementTree as ET
+
+import benchexec.util as util
+import benchexec.tools.template
+import benchexec.result as result
+
+class Tool(benchexec.tools.template.BaseTool):
+    """
+    Tool info for Dartagnan (http://https://github.com/hernanponcedeleon/Dat3M).
+    """
+
+    def executable(self):
+        return util.find_executable("java")
+
+    def name(self):
+        return "Dartagnan"
+
+    def cmdline(self, executable, options, tasks, propertyfile=None, rlimits={}):
+        return [executable, "-jar", "svcomp/target/svcomp-2.0.4-jar-with-dependencies.jar"] + options + tasks
+
+    def determine_result(self, returncode, returnsignal, output, isTimeout):
+        status = result.RESULT_ERROR
+        if len(output) > 0:
+            result_str = output[-1].strip()
+            if "FAIL" in result_str:
+                status = result.RESULT_FALSE_PROP
+            elif "PASS" in result_str:
+                status = result.RESULT_TRUE_PROP
+            elif "UNKNOWN" in result_str:
+                status = result.RESULT_UNKNOWN
+        return status
