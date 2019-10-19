9d99a12163e29b14fead6efcd001e58e3eb2f4e0
Philipp Wendler <uni@philippwendler.de>
2019-10-19 16:22:34 +0200

    fix source format

--- benchexec/tools/jdart.py
+++ benchexec/tools/jdart.py
@@ -27,20 +27,22 @@ class Tool(benchexec.tools.template.BaseTool):
     (https://github.com/tudo-aqua/jdart).
     """
 
-    REQUIRED_PATHS = ["jconstraints-extensions",
-                      "lib",
-                      "z3",
-                      "basic.jpf",
-                      "jdk8u222-b10",
-                      "jconstraints-0.9.2-SNAPSHOT.jar",
-                      "jpf-annotations.jar",
-                      "jpf-classes.jar",
-                      "jpf.jar",
-                      "jpf-jdart-annotations.jar",
-                      "jpf-jdart-classes.jar",
-                      "jpf-jdart.jar",
-                      "RunJPF.jar",
-                      "version.txt"]
+    REQUIRED_PATHS = [
+        "jconstraints-extensions",
+        "lib",
+        "z3",
+        "basic.jpf",
+        "jdk8u222-b10",
+        "jconstraints-0.9.2-SNAPSHOT.jar",
+        "jpf-annotations.jar",
+        "jpf-classes.jar",
+        "jpf.jar",
+        "jpf-jdart-annotations.jar",
+        "jpf-jdart-classes.jar",
+        "jpf-jdart.jar",
+        "RunJPF.jar",
+        "version.txt",
+    ]
 
     def executable(self):
         return util.find_executable("run-jdart.sh")
