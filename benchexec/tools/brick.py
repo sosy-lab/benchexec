63be2defbc5a347ed07ec0287f790d3c30851b55
brick-tool <brick_tool@163.com>
2019-10-20 19:38:17 +0800

    update tool-info

--- benchexec/tools/brick.py
+++ benchexec/tools/brick.py
@@ -2,7 +2,7 @@
 BenchExec is a framework for reliable benchmarking.
 This file is part of BenchExec.
 
-Copyright (C) 2007-2015  Dirk Beyer
+Copyright (C) 2007-2019  Dirk Beyer
 All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,32 +17,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import logging
-import os
+
 import benchexec.util as util
 import benchexec.tools.template
 import benchexec.result as result
 
+
 class Tool(benchexec.tools.template.BaseTool):
     """
     Tool info for BRICK
+    https://github.com/brick-tool-dev/brick-tool
     """
-    
+
     REQUIRED_PATHS = ["bin", "lib"]
 
     def executable(self):
         return util.find_executable("bin/brick")
 
-
     def name(self):
         return "BRICK"
-
 
     def cmdline(self, executable, options, tasks, propertyfile, rlimits):
         self.options = options
 
         return [executable] + options + tasks
-    
+
     def version(self, executable):
         return self._version_from_tool(executable, arg="--version")
 
@@ -53,22 +52,20 @@ class Tool(benchexec.tools.template.BaseTool):
         )
 
     def determine_result(self, returncode, returnsignal, output, isTimeout):
-        if len(output) > 0:
-            logging.info(output)
-            status = result.RESULT_ERROR
+        status = result.RESULT_ERROR
 
-            for line in output:
-                if line == "VERIFICATION SUCCESSFUL\n":
-                    status = result.RESULT_TRUE_PROP
-                    break
-                elif line == "VERIFICATION FAILED\n":
-                    status = result.RESULT_FALSE_REACH
-                    break
-                elif line == "VERIFICATION UNKNOWN\n":
-                    status = result.RESULT_UNKNOWN
-                    break
-
-        else:
-            status = result.RESULT_ERROR
+        for line in output:
+            if line == "VERIFICATION SUCCESSFUL\n":
+                status = result.RESULT_TRUE_PROP
+                break
+            elif line == "VERIFICATION FAILED\n":
+                status = result.RESULT_FALSE_REACH
+                break
+            elif (
+                line == "VERIFICATION UNKNOWN\n"
+                or line == "VERIFICATION BOUNDED TRUE\n"
+            ):
+                status = result.RESULT_UNKNOWN
+                break
 
         return status
