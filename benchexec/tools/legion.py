c87114afb7bc976534ab6601c2ef9c6c42fc8fde
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2019-10-21 13:10:45 +0200

    Comply with test-comp requirements

--- benchexec/tools/legion.py
+++ benchexec/tools/legion.py
@@ -1,6 +1,27 @@
+"""
+BenchExec is a framework for reliable benchmarking.
+This file is part of BenchExec.
+
+Copyright (C) 2007-2015  Dirk Beyer
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import benchexec.result as result
 import benchexec.util as util
 import benchexec.tools.template
+
 
 class Tool(benchexec.tools.template.BaseTool):
     """
@@ -17,7 +38,10 @@ class Tool(benchexec.tools.template.BaseTool):
     ]
 
     def executable(self):
-        return util.find_executable('legion-sv')
+        return util.find_executable("legion-sv")
+
+    def version(self, executable):
+        "0.1-testcomp2020"
 
     def name(self):
-        return 'Legion'
+        return "Legion"
