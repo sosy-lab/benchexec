34c00ece378e6bae22644c1ec4b1e82eb3171930
Hernan Ponce de Leon <ponce@fortiss.org>
2019-10-30 06:09:12 +0100

    added output folder

--- benchexec/tools/dartagnan.py
+++ benchexec/tools/dartagnan.py
@@ -18,31 +18,26 @@ import benchexec.util as util
 import benchexec.tools.template
 import benchexec.result as result
 
+
 class Tool(benchexec.tools.template.BaseTool):
     """
     Tool info for Dartagnan (https://github.com/hernanponcedeleon/Dat3M).
     """
 
-    REQUIRED_PATHS = [
-        "svcomp/target",
-        "dartagnan/target",
-        "cat",
-        "lib",
-        "smack",
-    ]
+    REQUIRED_PATHS = ["svcomp/target", "dartagnan/target", "cat", "lib", "smack", "output"]
 
     def executable(self):
         return util.find_executable("./Dartagnan-SVCOMP.sh")
 
     def name(self):
         return "Dartagnan"
-    
+
     def cmdline(self, executable, options, tasks, propertyfile=None, rlimits={}):
         return [executable] + options + tasks
 
     def version(self, executable):
         return self._version_from_tool(executable)
-    
+
     def determine_result(self, returncode, returnsignal, output, isTimeout):
         status = result.RESULT_ERROR
         if output:
@@ -57,4 +52,3 @@ class Tool(benchexec.tools.template.BaseTool):
 
     def program_files(self, executable):
         return [executable] + self.REQUIRED_PATHS
-
