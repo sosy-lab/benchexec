5a700efacff4a577709ce864c80550441d4c412d
Philipp Wendler <uni@philippwendler.de>
2019-11-04 18:07:36 +0100

    Run snapshot tests for each of our test tables
    Also run snapshot tests on both the Summary tab (with the header data)
    and the Table tab.
    
    Fixes #478.

--- benchexec/tablegenerator/react-table/src/tests/Summary.test.js
+++ benchexec/tablegenerator/react-table/src/tests/Summary.test.js
@@ -4,35 +4,23 @@
  * This file is part of BenchExec.
  * Copyright (C) Dirk Beyer. All rights reserved.
  */
-// react standard
 import React from "react";
-import renderer from "react-test-renderer";
-
-// components
-import Overview from "../components/Overview";
 import Summary from "../components/Summary.js";
+
+import { test_snapshot_of } from "./utils.js";
 
 // mock uniqid to have consistent names
 // https://stackoverflow.com/a/44538270/396730
 jest.mock("uniqid", () => i => i + "uniqid");
 
-it("Render Summary", () => {
-  window.data = require("../data/data.json");
-
-  const overview = renderer.create(<Overview />).getInstance();
-  const component = renderer
-    .create(
-      <Summary
-        tools={overview.originalTools}
-        tableHeader={overview.tableHeader}
-        selectColumn={overview.toggleSelectColumns}
-        stats={overview.stats}
-        prepareTableValues={overview.prepareTableValues}
-        getRunSets={overview.getRunSets}
-        changeTab={overview.changeTab}
-      />
-    )
-    .toJSON();
-
-  expect(component).toMatchSnapshot();
-});
+test_snapshot_of("Render Summary", overview => (
+  <Summary
+    tools={overview.originalTools}
+    tableHeader={overview.tableHeader}
+    selectColumn={overview.toggleSelectColumns}
+    stats={overview.stats}
+    prepareTableValues={overview.prepareTableValues}
+    getRunSets={overview.getRunSets}
+    changeTab={overview.changeTab}
+  />
+));
