141fc5382f03f13bc21139953f8597f0d21cbd94
Dennis Simon <dennis.simon@dn-designcanvas.com>
2019-12-03 17:15:23 +0100

    Revert changes

--- benchexec/tablegenerator/react-table/src/tests/mocks/importsLoader.js
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0
- *
- * BenchExec is a framework for reliable benchmarking.
- * This file is part of BenchExec.
- * Copyright (C) Dirk Beyer. All rights reserved.
- */
-
-module.exports = "MOCKED";
--- benchexec/tablegenerator/react-table/src/vendor/zip.js/index.js
+++ benchexec/tablegenerator/react-table/src/vendor/zip.js/index.js
@@ -15,10 +15,4 @@
 require("imports-loader?this=>window!./zip.js");
 require("imports-loader?this=>window!./zip-ext.js");
 require("imports-loader?this=>window!./inflate.js");
-
-if (typeof zip !== "undefined") {
-  zip.useWebWorkers = false;
-  module.exports = zip;
-} else {
-  module.exports = {};
-}
+zip.useWebWorkers = false;
