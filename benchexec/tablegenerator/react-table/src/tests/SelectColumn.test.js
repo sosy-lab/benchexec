d3c1c3292a4a02ca9485bb8313b72c54130f1a0f
Martin <yankov@cip.ifi.lmu.de>
2020-03-04 11:50:57 +0100

    renamed App.test to SelectColumn.test

--- benchexec/tablegenerator/react-table/src/tests/SelectColumn.test.js
+++ benchexec/tablegenerator/react-table/src/tests/SelectColumn.test.js
@@ -4,12 +4,14 @@
  * This file is part of BenchExec.
  * Copyright (C) Dirk Beyer. All rights reserved.
  */
-import App from "../App.js";
 import React from "react";
+import SelectColumn from "../App.js";
 import { test_snapshot_of } from "./utils.js";
 
 // mock uniqid to have consistent names
 // https://stackoverflow.com/a/44538270/396730
 jest.mock("uniqid", () => i => i + "uniqid");
 
-test_snapshot_of("Render App", overview => <App data={overview.data} />);
+test_snapshot_of("Render App", overview => (
+  <SelectColumn data={overview.data} />
+));
