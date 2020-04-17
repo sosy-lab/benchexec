b6e4e6abd6d57d1123839074b8735c774e14cc55
Dennis Simon <dennism.simon94@gmail.com>
2020-04-17 12:23:12 +0200

    Add simple filter fields

--- benchexec/tablegenerator/react-table/src/components/FilterBox/FilterBox.js
+++ benchexec/tablegenerator/react-table/src/components/FilterBox/FilterBox.js
@@ -3,16 +3,45 @@ import FilterContainer from "./FilterContainer";
 
 const linkDataToTool = (toolIdx, columns, data) => {
   const out = columns;
+  //prepare columns
+  for (const column of out) {
+    column.data = [];
+    column.distincts = {};
+    column.min = Infinity;
+    column.max = -Infinity;
+  }
   for (const row of data) {
     const toolResult = row.results[toolIdx];
     for (const index in out) {
-      if (!out[index].data) {
-        out[index].data = [];
+      const value = toolResult.values[index];
+      let { min, max, type } = out[index];
+      out[index].data.push(value);
+
+      if (value.raw === undefined) {
+        continue;
       }
-      out[index].data.push(toolResult.values[index]);
+      // aggregate data
+      if (type === "status" || type === "text") {
+        // using a plain object as a simple representation of a set
+        out[index].distincts[value.raw] = true;
+      } else {
+        min = Number(min);
+        max = Number(max);
+        if (value.raw < min) {
+          min = value.raw;
+        }
+        if (value.raw > max) {
+          max = value.raw;
+        }
+        out[index].min = min;
+        out[index].max = max;
+      }
     }
   }
-  return out;
+
+  // turning the distincts set into an array
+  console.log(out);
+  return out.map((col) => ({ ...col, distincts: Object.keys(col.distincts) }));
 };
 
 const extractFilterValues = (columns) => {
--- benchexec/tablegenerator/react-table/src/components/FilterBox/FilterCard.js
+++ benchexec/tablegenerator/react-table/src/components/FilterBox/FilterCard.js
@@ -12,6 +12,9 @@ export default class FilterCard extends React.Component {
           : "",
       value: null,
       active: true,
+      selectedDistincts: [],
+      currentMin: (props.filter && props.filter.min) || 0,
+      currentMax: (props.filter && props.filter.max) || 0,
     };
   }
 
@@ -22,6 +25,7 @@ export default class FilterCard extends React.Component {
 
   render() {
     const { filter, editable, availableFilters } = this.props;
+    console.log("FilterCard received", filter);
     const filterAddSelection = () => (
       <>
         <select
@@ -51,8 +55,109 @@ export default class FilterCard extends React.Component {
       </div>
     );
 
+    const makeFilterBody = (filter) => {
+      if (!filter) {
+        return null;
+      }
+      const { title, type, distincts, min, max } = filter;
+      let body;
+      if (type === "status") {
+        body = (
+          <>
+            <b>Category</b>
+            <ul className="filter-card--body--list">
+              <li>
+                <input type="checkbox" name="cat-correct" />
+                <label for="cat-correct">correct</label>
+              </li>
+              <li>
+                <input type="checkbox" name="cat-error" />
+                <label for="cat-correct">error</label>
+              </li>
+              <li>
+                <input type="checkbox" name="cat-unknown" />
+                <label for="cat-correct">unknown</label>
+              </li>
+              <li>
+                <input type="checkbox" name="cat-wrong" />
+                <label for="cat-correct">wrong</label>
+              </li>
+            </ul>
+            <b>Status</b>
+            <ul className="filter-card--body--list">
+              {distincts.map((status) => (
+                <li key={status}>
+                  <input type="checkbox" name={`stat-${status}`} />
+                  <label for={`stat-${status}`}>{status}</label>
+                </li>
+              ))}
+            </ul>
+          </>
+        );
+      } else if (type === "text") {
+        body = (
+          <ul className="filter-card--body--list">
+            {distincts.map((status) => (
+              <li key={status}>
+                <input type="checkbox" name={`stat-${status}`} />
+                <label for={`text-${status}`}>{status}</label>
+              </li>
+            ))}
+          </ul>
+        );
+      } else if (type === "count") {
+        body = (
+          <>
+            <label for={`${title}-${type}-min`}>Minimum</label>
+            <input
+              type="number"
+              min={min}
+              max={this.state.currentMax}
+              name={`${title}-${type}-min`}
+              onBlur={(e) => this.setState({ currentMin: e.target.value })}
+            />
+            <label for={`${title}-${type}-max`}>Maximum</label>
+            <input
+              type="number"
+              min={this.state.currentMin}
+              max={max}
+              onBlur={(e) => this.setState({ currentMax: e.target.value })}
+              name={`${title}-${type}-max`}
+            />
+          </>
+        );
+      } else if (type === "measure") {
+        body = (
+          <>
+            <label for={`${title}-${type}-min`}>Minimum</label>
+            <input
+              type="number"
+              min={min}
+              max={this.state.currentMax}
+              name={`${title}-${type}-min`}
+              value={this.state.currentMin}
+              onChange={(e) => this.setState({ currentMin: e.target.value })}
+            />
+            <label for={`${title}-${type}-max`}>Maximum</label>
+            <input
+              type="number"
+              min={this.state.currentMin}
+              max={max}
+              value={this.state.currentMax}
+              onChange={(e) => this.setState({ currentMax: e.target.value })}
+              name={`${title}-${type}-max`}
+            />
+          </>
+        );
+      }
+      return <div className="filter-card--body">{body}</div>;
+    };
+
     return (
-      <div className="filter-card">{makeHeader(this.props.name, editable)}</div>
+      <div className="filter-card">
+        {makeHeader(this.props.name, editable)}
+        {makeFilterBody(this.props.filter)}
+      </div>
     );
   }
 }
