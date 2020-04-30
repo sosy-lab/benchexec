3fd357382580854952ddb0a2d173941b3bdd7f29
Dennis Simon <dennism.simon94@gmail.com>
2020-04-30 11:44:56 +0200

    Add slider and handling of current values

--- benchexec/tablegenerator/react-table/src/components/FilterBox/FilterCard.js
+++ benchexec/tablegenerator/react-table/src/components/FilterBox/FilterCard.js
@@ -1,50 +1,91 @@
 import React from "react";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Slider, { createSliderWithTooltip } from "rc-slider";
+
+const Range = createSliderWithTooltip(Slider.Range);
 
 export default class FilterCard extends React.Component {
   constructor(props) {
     super(props);
+    const { value } = props.filter || { value: undefined };
+    let currentMin = 0;
+    let currentMax = 0;
+    if (value && value.includes(":")) {
+      const res = this.handleMinMaxValue(value);
+      currentMin = res.min;
+      currentMax = res.max;
+    }
     this.state = {
       title:
         props.availableFilters && props.availableFilters.length
           ? props.availableFilters[0].title
           : "",
       value: null,
+      idx: 0,
       active: true,
       selectedDistincts: [],
-      currentMin: (props.filter && props.filter.min) || 0,
-      currentMax: (props.filter && props.filter.max) || 0,
+      currentMin,
+      currentMax,
     };
   }
 
-  sendFilterUpdate() {
+  sendFilterUpdate(value) {
     this.props.onFilterUpdate({
-      ...this.state,
+      value: value,
       title: this.state.title || this.props.title,
     });
-    console.log("Filtercard sent", this.state);
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log("filtercard updates", { prevProps, newProps: this.props });
+    if (!this.props.filter) {
+      return;
+    }
+    if (
+      !prevProps.filter ||
+      prevProps.filter.value !== this.props.filter.value
+    ) {
+      const { value } = this.props.filter;
+      console.log("filtercard decided to update: ", value);
+      if (value && value.includes(":")) {
+        const { min, max } = this.handleMinMaxValue(value);
+        console.log("settings state", { currentMin: min, currentMax: max });
+        this.setState({ currentMin: min, currentMax: max });
+      }
+    }
+  }
+
+  handleMinMaxValue(value) {
+    const { min: propMin, max: propMax } = this.props.filter || {
+      min: 0,
+      max: Infinity,
+    };
+    console.log("handleMinMaxValue", { propMin, propMax });
+    const [vMin, vMax] = value.split(":");
+    return {
+      min: vMin.trim() !== "" ? Number(vMin) : propMin,
+      max: vMax.trim() !== "" ? Number(vMax) : propMax,
+    };
   }
 
   render() {
     const { filter, editable, availableFilters } = this.props;
-    console.log("FilterCard received", filter);
+    // console.log("FilterCard received", filter);
     const filterAddSelection = () => (
       <>
         <select
           class="filter-selection"
-          onChange={(e) =>
-            this.setState({ title: e.target.value, active: true })
-          }
+          onChange={(e) => this.setState({ idx: e.target.value, active: true })}
         >
-          {availableFilters.map((i) => (
-            <option value={i.title}>{i.display_title}</option>
+          {availableFilters.map(({ idx, display_title }) => (
+            <option value={idx}>{display_title}</option>
           ))}
         </select>
         <FontAwesomeIcon
           className="check-button"
           icon={faCheck}
-          onClick={() => this.sendFilterUpdate()}
+          onClick={() => this.props.addFilter(this.state.idx)}
         />
       </>
     );
@@ -70,35 +111,62 @@ export default class FilterCard extends React.Component {
       if (!filter) {
         return null;
       }
-      const { title, type, distincts, min, max } = filter;
+      const {
+        title,
+        type,
+        distincts,
+        min,
+        max,
+        categories,
+        statuses,
+        value,
+      } = filter;
       let body;
       if (type === "status") {
         body = (
           <>
             <b>Category</b>
             <ul className="filter-card--body--list">
-              <li>
-                <input type="checkbox" name="cat-correct" />
-                <label for="cat-correct">correct</label>
-              </li>
-              <li>
-                <input type="checkbox" name="cat-error" />
-                <label for="cat-correct">error</label>
-              </li>
-              <li>
-                <input type="checkbox" name="cat-unknown" />
-                <label for="cat-correct">unknown</label>
-              </li>
-              <li>
-                <input type="checkbox" name="cat-wrong" />
-                <label for="cat-correct">wrong</label>
-              </li>
+              {categories.map((category) => (
+                <li key={category}>
+                  <input
+                    type="checkbox"
+                    name={`cat-${category}`}
+                    checked={category === value}
+                    onChange={({ target: { checked } }) => {
+                      console.log({ checked, category });
+                      if (checked) {
+                        this.setState({ value: category });
+                        this.sendFilterUpdate(category);
+                      } else {
+                        this.setState({ value: null });
+                        this.sendFilterUpdate(null);
+                      }
+                    }}
+                  />
+                  <label for={`cat-${category}`}>{category}</label>
+                </li>
+              ))}
             </ul>
             <b>Status</b>
             <ul className="filter-card--body--list">
-              {distincts.map((status) => (
+              {statuses.map((status) => (
                 <li key={status}>
-                  <input type="checkbox" name={`stat-${status}`} />
+                  <input
+                    type="checkbox"
+                    name={`stat-${status}`}
+                    checked={status === value}
+                    onChange={({ target: { checked } }) => {
+                      console.log({ checked, status });
+                      if (checked) {
+                        this.setState({ value: status });
+                        this.sendFilterUpdate(status);
+                      } else {
+                        this.setState({ value: null });
+                        this.sendFilterUpdate(null);
+                      }
+                    }}
+                  />
                   <label for={`stat-${status}`}>{status}</label>
                 </li>
               ))}
@@ -110,54 +178,94 @@ export default class FilterCard extends React.Component {
           <ul className="filter-card--body--list">
             {distincts.map((status) => (
               <li key={status}>
-                <input type="checkbox" name={`stat-${status}`} />
+                <input
+                  type="checkbox"
+                  name={`stat-${status}`}
+                  checked={status === value}
+                />
                 <label for={`text-${status}`}>{status}</label>
               </li>
             ))}
           </ul>
         );
-      } else if (type === "count") {
+      } else {
         body = (
           <>
-            <label for={`${title}-${type}-min`}>Minimum</label>
-            <input
-              type="number"
+            <div className="filter-card--range-container">
+              <b>{min}</b>
+              <b>{max}</b>
+            </div>
+            <Range
               min={min}
-              max={this.state.currentMax}
-              name={`${title}-${type}-min`}
-              onBlur={(e) => this.setState({ currentMin: e.target.value })}
-            />
-            <label for={`${title}-${type}-max`}>Maximum</label>
-            <input
-              type="number"
-              min={this.state.currentMin}
               max={max}
-              onBlur={(e) => this.setState({ currentMax: e.target.value })}
-              name={`${title}-${type}-max`}
+              defaultValue={[min, max]}
+              value={[this.state.currentMin, this.state.currentMax]}
+              onChange={([nMin, nMax]) =>
+                this.setState({ currentMin: nMin, currentMax: nMax })
+              }
+              onAfterChange={([nMin, nMax]) => {
+                this.setState({
+                  currentMin: nMin,
+                  currentMax: nMax,
+                  value: `${nMin}:${nMax}`,
+                });
+                this.sendFilterUpdate(`${nMin}:${nMax}`);
+              }}
             />
-          </>
-        );
-      } else if (type === "measure") {
-        body = (
-          <>
-            <label for={`${title}-${type}-min`}>Minimum</label>
-            <input
-              type="number"
-              min={min}
-              max={this.state.currentMax}
-              name={`${title}-${type}-min`}
-              value={this.state.currentMin}
-              onChange={(e) => this.setState({ currentMin: e.target.value })}
-            />
-            <label for={`${title}-${type}-max`}>Maximum</label>
-            <input
-              type="number"
-              min={this.state.currentMin}
-              max={max}
-              value={this.state.currentMax}
-              onChange={(e) => this.setState({ currentMax: e.target.value })}
-              name={`${title}-${type}-max`}
-            />
+            <div className="filter-card--range-input-fields">
+              <label
+                className="range-input-fields--min"
+                for={`inp-${title}-min`}
+              >
+                minimum
+              </label>
+              <label
+                className="range-input-fields--max"
+                for={`inp-${title}-max`}
+              >
+                maximum
+              </label>
+              <input
+                type="number"
+                min={min}
+                name={`inp-${title}-min`}
+                value={this.state.currentMin}
+                onChange={({ target: { value } }) => {
+                  if (value > this.state.currentMax) {
+                    this.setState({
+                      currentMax: value,
+                      currentMin: this.state.currentMax,
+                      value: `${this.state.currentMax}:${value}`,
+                    });
+                  } else {
+                    this.setState({
+                      currentmin: value,
+                      value: `${value}:${this.state.currentMax}`,
+                    });
+                  }
+                }}
+              />
+              <input
+                type="number"
+                min={max}
+                name={`inp-${title}-max`}
+                value={this.state.currentMax}
+                onChange={({ target: { value } }) => {
+                  if (value < this.state.currentMin) {
+                    this.setState({
+                      currentMax: this.state.currentMin,
+                      currentMin: value,
+                      value: `${value}:${this.state.currentMin}`,
+                    });
+                  } else {
+                    this.setState({
+                      currentMax: value,
+                      value: `${this.state.currentMin}:${value}`,
+                    });
+                  }
+                }}
+              />
+            </div>
           </>
         );
       }
