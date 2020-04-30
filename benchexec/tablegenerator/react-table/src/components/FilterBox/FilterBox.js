f81d4229f1444acb62d41f67e26044ad18a3b22c
Dennis Simon <dennism.simon94@gmail.com>
2020-04-30 11:45:46 +0200

    Add filter linking between table and FilterBox

--- benchexec/tablegenerator/react-table/src/components/FilterBox/FilterBox.js
+++ benchexec/tablegenerator/react-table/src/components/FilterBox/FilterBox.js
@@ -1,122 +1,93 @@
 import React from "react";
 import FilterContainer from "./FilterContainer";
-import { faBreadSlice } from "@fortawesome/free-solid-svg-icons";
-
-const linkDataToTool = (toolIdx, columns, data) => {
-  const out = columns;
-  //prepare columns
-  for (const column of out) {
-    column.data = [];
-    column.distincts = {};
-    column.min = Infinity;
-    column.max = -Infinity;
-  }
-  for (const row of data) {
-    const toolResult = row.results[toolIdx];
-    for (const index in out) {
-      const value = toolResult.values[index];
-      let { min, max, type } = out[index];
-      out[index].data.push(value);
-
-      if (value.raw === undefined) {
-        continue;
-      }
-      // aggregate data
-      if (type === "status" || type === "text") {
-        // using a plain object as a simple representation of a set
-        out[index].distincts[value.raw] = true;
-      } else {
-        min = Number(min);
-        max = Number(max);
-        if (value.raw < min) {
-          min = value.raw;
-        }
-        if (value.raw > max) {
-          max = value.raw;
-        }
-        out[index].min = min;
-        out[index].max = max;
-      }
-    }
-  }
-
-  // turning the distincts set into an array
-  console.log(out);
-  return out.map((col) => ({ ...col, distincts: Object.keys(col.distincts) }));
-};
-
-const extractFilterValues = (columns) => {
-  const out = {};
-  for (const column of columns) {
-    out[column.title] = { ...column, filtered: false, value: null };
-  }
-  return out;
-};
 
 export default class FilterBox extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
 
-    const { tools, data } = props;
-
-    const preppedTools = tools.map(
-      ({ niceName, date, tool, isVisible, columns }, idx) => ({
-        name: `${tool} ${date} ${niceName}`,
-        isVisible,
-        columns: linkDataToTool(idx, columns, data),
-        filters: extractFilterValues(columns),
-        index: idx,
-      }),
-    );
+    const { filterable, filtered } = props;
 
     this.state = {
-      preppedTools,
+      filterable,
+      filters: this.createFiltersFromReactTableStructure(filtered),
     };
+  }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.filtered !== this.props.filtered) {
+      this.setState({
+        filters: this.createFiltersFromReactTableStructure(this.props.filtered),
+      });
+    }
     console.log(this.state);
   }
-  updateFilters(idx, data) {
-    const preppedTools = this.state.preppedTools;
-    console.log("box received", data);
-    preppedTools[idx].columns = data;
-    this.setState({ preppedTools });
-    const filtered = [];
-    for (const row of this.props.data) {
-      const results = row.results;
-      let push = true;
-      for (const tool of preppedTools) {
-        if (!push) {
-          break;
-        }
-        const toolSection = results[tool.index];
-        for (const colIdx in tool.columns) {
-          const col = tool.columns[colIdx];
-          if (col.currentMin !== 0 && col.currentMax !== 0) {
-            const val = Number(toolSection.values[colIdx].raw);
-            if (val < col.currentMin || val > col.currentMax) {
-              push = false;
-              break;
-            }
-          }
-        }
-      }
-      if (push) {
-        filtered.push({ _original: row });
-      }
+
+  createFiltersFromReactTableStructure(filters) {
+    if (!filters || !filters.length) {
+      return [];
     }
-    this.props.setFilter(filtered);
+
+    const out = [];
+
+    for (const { id, value } of filters) {
+      const [tool, title, col] = id.split("_");
+      const toolArr = out[tool] || [];
+      toolArr[col] = { title, value };
+      out[tool] = toolArr;
+    }
+
+    return out;
+  }
+
+  flattenFilterStructure() {
+    return Object.values(Object.values(this.state.filters));
+  }
+
+  sendFilters(filter) {
+    const filters = filter.filter((i) => i !== null && i !== undefined);
+
+    console.log({ filters });
+
+    this.props.setFilter(
+      filters
+        .map((tool, toolIdx) => {
+          return tool.map((col, colIdx) => {
+            return {
+              id: `${toolIdx}_${col.title}_${colIdx}`,
+              value: col.value,
+            };
+          });
+        })
+        .flat()
+        .filter((i) => i !== null && i !== undefined),
+    );
+  }
+
+  updateFilters(toolIdx, columnIdx, data) {
+    console.log({ toolIdx, columnIdx, data });
+    //this.props.setFilter(newFilter);
+    const newFilters = [...this.state.filters];
+    newFilters[toolIdx] = newFilters[toolIdx] || [];
+    newFilters[toolIdx][columnIdx] = data;
+    this.setState({ filters: newFilters });
+    console.log(this.state.filters);
+    this.sendFilters(newFilters);
   }
 
   render() {
     return (
       <div className="filterBox">
-        {this.state.preppedTools.map((tool) => {
+        {this.state.filterable.map((tool, idx) => {
           return (
             <FilterContainer
-              updateFilters={(data) => this.updateFilters(tool.index, data)}
+              updateFilters={(data, columnIndex) =>
+                this.updateFilters(idx, columnIndex, data)
+              }
+              currentFilters={this.state.filters[idx] || []}
               toolName={tool.name}
               filters={tool.columns}
+              key={`filtercontainer-${idx}`}
             />
           );
         })}
--- benchexec/tablegenerator/react-table/src/components/FilterBox/FilterContainer.js
+++ benchexec/tablegenerator/react-table/src/components/FilterBox/FilterContainer.js
@@ -8,60 +8,87 @@ export default class FilterContainer extends React.Component {
     super(props);
 
     const { filters, toolName } = props;
-    this.state = { filters, toolName, addingFilter: false };
+    this.state = { filters, toolName, addingFilter: false, numCards: 0 };
   }
 
   getActiveFilters() {
-    return this.state.filters.filter((item) => item.filtered);
+    return this.state.filters
+      .filter((item) => item.filtering)
+      .sort((a, b) => a.numCards - b.numCards);
   }
 
-  setFilter({ title, active, currentMin, currentMax }) {
+  setFilter({ title, value }, idx) {
     console.log("Container received", {
       title,
-      active,
-      currentMin,
-      currentMax,
+      value,
+      idx,
     });
-    const newFilters = this.state.filters.map((filter) =>
-      filter.title === title
-        ? {
-            ...filter,
-            filtered: active,
-            currentMin: Number(currentMin),
-            currentMax: Number(currentMax),
-          }
-        : filter,
-    );
-    this.setState({
-      addingFilter: false,
-      filters: newFilters,
-    });
+    this.props.updateFilters({ title, value }, idx);
+  }
 
-    this.props.updateFilters(newFilters);
+  addFilter(idx) {
+    const { filters: newFilterState, numCards } = this.state;
+    console.log({ newFilterState, idx });
+    newFilterState[idx].filtering = true;
+    newFilterState[idx].numCards = numCards;
+
+    this.setState({
+      filters: newFilterState,
+      addingFilter: false,
+      numCards: numCards + 1,
+    });
+  }
+
+  removeFilter(idx) {
+    const newFilterState = this.state.filters;
+    newFilterState[idx].filtering = false;
+    newFilterState[idx].value = null;
+
+    this.setState({ filters: newFilterState });
+  }
+
+  componentDidUpdate({ currentFilters: prevFilters }) {
+    const { currentFilters } = this.props;
+    if (prevFilters !== currentFilters) {
+      // update set filters
+      const { filters } = this.state;
+      filters.forEach((item) => (item.filtering = false));
+      for (const idx in currentFilters) {
+        filters[idx] = {
+          ...filters[idx],
+          ...currentFilters[idx],
+          filtering: currentFilters[idx].value !== "all ",
+        };
+      }
+      this.setState({ filters });
+    }
   }
 
   render() {
+    console.log({ currentFilters: this.props.currentFilters });
     const filters = this.getActiveFilters();
+    console.log({ activeFilters: filters });
     console.log("container rendered");
     return (
       <div className="filterBox--container">
         <h4>{this.state.toolName}</h4>
         {filters.length === 0
           ? "No Filters set"
-          : filters.map((filter) => (
+          : filters.map((filter, idx) => (
               <FilterCard
-                onFilterUpdate={(vals) => this.setFilter(vals)}
+                onFilterUpdate={(val) => this.setFilter(val, filter.idx)}
                 title={filter.display_title}
                 filter={filter}
-                key={filter.title}
+                key={filter.numCards}
               />
             ))}
         <br />
         <br />
         {this.state.addingFilter ? (
           <FilterCard
-            availableFilters={this.state.filters.filter((i) => !i.filtered)}
+            availableFilters={this.state.filters.filter((i) => !i.filtering)}
             editable="true"
+            addFilter={(idx) => this.addFilter(idx)}
             onFilterUpdate={(vals) => this.setFilter(vals)}
           />
         ) : (
