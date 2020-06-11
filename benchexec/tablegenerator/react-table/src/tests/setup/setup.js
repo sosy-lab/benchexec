453b9c28f87d026997a3de974a41a8fc0108ed3f
Dennis Simon <dennism.simon94@gmail.com>
2020-06-11 16:02:49 +0200

    Move uniqid mock to setup and fix ReactTable test

--- benchexec/tablegenerator/react-table/src/tests/setup/setup.js
+++ benchexec/tablegenerator/react-table/src/tests/setup/setup.js
@@ -2,4 +2,8 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+// mock uniqid to have consistent names
+// https://stackoverflow.com/a/44538270/396730
+jest.mock("uniqid", () => (i) => i + "uniqid");
+
 configure({ adapter: new Adapter() });
