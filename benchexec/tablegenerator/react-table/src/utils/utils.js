9556456522aa569447c8a6b60b87f0b0bc47b50a
lachnerm <michael.lachner@outlook.de>
2020-07-08 16:06:50 +0200

    Escape and unescape the URL when setting or retrieving it Currently it was possible for characters to be set in the URL via parameters that will actually break the URL. With escaping the URL string before setting (and respectively after retrieving), this is not possible any more. Fixes #584.

--- benchexec/tablegenerator/react-table/src/utils/utils.js
+++ benchexec/tablegenerator/react-table/src/utils/utils.js
@@ -151,7 +151,7 @@ const EXTENDED_DISCRETE_COLOR_RANGE = [
  * @param {String} [str]
  */
 const getHashSearch = (str) => {
-  const urlParts = (str || document.location.href).split("?");
+  const urlParts = (str || decodeURI(document.location.href)).split("?");
   const search = urlParts.length > 1 ? urlParts[1] : undefined;
   if (search === undefined || search.length === 0) {
     return {};
@@ -179,7 +179,7 @@ const setHashSearch = (
   const url = (baseUrl || document.location.href).split("?")[0];
   const pairs = Object.keys(params).map((key) => `${key}=${params[key]}`);
   const searchString = `?${pairs.join("&")}`;
-  const hrefString = `${url}${searchString}`;
+  const hrefString = encodeURI(`${url}${searchString}`);
   if (returnString) {
     return hrefString;
   }
