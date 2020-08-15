d52051b30417a8ad74e2c4ddc272203d67a9fdc1
Dennis Simon <dennism.simon94@gmail.com>
2020-08-15 10:51:48 +0200

    Add license info

--- benchexec/tablegenerator/react-table/scripts/workerDataUrls.js
+++ benchexec/tablegenerator/react-table/scripts/workerDataUrls.js
@@ -1,3 +1,10 @@
+// This file is part of BenchExec, a framework for reliable benchmarking:
+// https://github.com/sosy-lab/benchexec
+//
+// SPDX-FileCopyrightText: 2019-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 const fs = require("fs");
 const path = require("path");
 
@@ -8,7 +15,16 @@ const template = "data:text/plain;base64,";
 
 const workerFiles = fs.readdirSync(workerFilePath);
 
-let output = "";
+let output = `
+// This file is part of BenchExec, a framework for reliable benchmarking:
+// https://github.com/sosy-lab/benchexec
+//
+// SPDX-FileCopyrightText: 2019-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+`;
 const workerNames = [];
 
 for (const worker of workerFiles) {
--- benchexec/tablegenerator/react-table/src/workers/workerDirector.js
+++ benchexec/tablegenerator/react-table/src/workers/workerDirector.js
@@ -1,3 +1,10 @@
+// This file is part of BenchExec, a framework for reliable benchmarking:
+// https://github.com/sosy-lab/benchexec
+//
+// SPDX-FileCopyrightText: 2019-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Content of ./scripts/stats.worker.js transformed into a data url to
 // deal with Chrome being unable to load WebWorkers when opened using
 // the file:// protocol https://stackoverflow.com/questions/21408510/chrome-cant-load-web-worker
