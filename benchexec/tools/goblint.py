c0361fb3e2a0342f11df31b29e31177dd35cc2cd
Simmo Saan <simmo.saan@gmail.com>
2020-09-17 09:54:18 +0000

    Update Goblint tool-info header

--- benchexec/tools/goblint.py
+++ benchexec/tools/goblint.py
@@ -1,6 +1,13 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import benchexec.util as util
 import benchexec.tools.template
 import benchexec.result as result
-import benchexec.util as util
 
 
 class Tool(benchexec.tools.template.BaseTool):
