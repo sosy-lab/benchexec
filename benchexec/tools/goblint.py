7cfb7d44b5309ca527104715dd5fde7fc689e96d
Simmo Saan <simmo.saan@gmail.com>
2020-09-20 12:59:07 +0000

    Add fixpoint ERROR result to Goblint tool-info

--- benchexec/tools/goblint.py
+++ benchexec/tools/goblint.py
@@ -36,6 +36,8 @@ class Tool(benchexec.tools.template.BaseTool):
                 return result.RESULT_TRUE_PROP
             elif line == "SV-COMP (unreach-call): false":
                 return result.RESULT_FALSE_REACH
+            elif "Fixpoint not reached" in line:
+                return result.RESULT_ERROR + " (fixpoint)"
             elif "Fatal error" in line:
                 if "Assertion failed" in line:
                     return "ASSERTION"
