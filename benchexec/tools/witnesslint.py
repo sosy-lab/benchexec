35b67e00e378df43a657c56bd751e7059ef45c38
SvenUmbricht <Sven.Umbricht@gmail.com>
2020-10-02 12:07:19 +0200

    Add tool info for the witness linter.

---
+++ benchexec/tools/witnesslint.py
@@ -0,0 +1,21 @@
+import benchexec.result as result
+import benchexec.tools.template
+
+
+class Tool(benchexec.tools.template.BaseTool2):
+    """
+    Tool info for the witness checker (witnesslint)
+    (https://github.com/sosy-lab/sv-witnesses)
+    """
+
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("witnesslint.py")
+
+    def name(self):
+        return "witnesslint"
+
+    def determine_result(self, run):
+        if run.exit_code.value == 0:
+            return result.RESULT_TRUE_PROP
+        else:
+            return result.RESULT_FALSE_PROP
