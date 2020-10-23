98ceab12674e3eb39711230dd5cc1476580df872
danieldietsch <daniel.dietsch@gmail.com>
2020-10-23 10:20:18 +0200

    ah, forgot one

--- benchexec/tools/ultimateautomizer.py
+++ benchexec/tools/ultimateautomizer.py
@@ -3,7 +3,8 @@
 #
 # SPDX-FileCopyrightText: 2015-2020 Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
 # SPDX-FileCopyrightText: 2015-2020 Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
-#
+# SPDX-FileCopyrightText: 2015-2020 Dirk Beyer <https://www.sosy-lab.org>
+
 # SPDX-License-Identifier: Apache-2.0
 
 from . import ultimate
