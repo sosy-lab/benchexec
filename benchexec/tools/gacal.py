904cc513c4db76b14a77092ed43932e3462634a1
Sudeep Kanav <sudeep.kanav@lmu.de>
2020-10-25 13:44:42 +0100

    updated TI for gacal

--- benchexec/tools/gacal.py
+++ benchexec/tools/gacal.py
@@ -10,7 +10,7 @@ import benchexec.tools.template
 import benchexec.result as result
 
 
-class Tool(benchexec.tools.template.BaseTool):
+class Tool(benchexec.tools.template.BaseTool2):
     """
     Tool info for GACAL.
     URL: https://gitlab.com/bquiring/sv-comp-submission
@@ -25,8 +25,8 @@ class Tool(benchexec.tools.template.BaseTool):
         "scripts",
     ]
 
-    def executable(self):
-        return util.find_executable("run-gacal.py")
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("run-gacal.py")
 
     def name(self):
         return "GACAL"
@@ -34,11 +34,17 @@ class Tool(benchexec.tools.template.BaseTool):
     def version(self, executable):
         return self._version_from_tool(executable)
 
-    def cmdline(self, executable, options, tasks, propertyfile, rlimits):
-        return [executable] + options + tasks
+    def cmdline(self, executable, options, task, rlimits):
+        data_model_param = util.get_data_model_from_task(
+            task, {"ILP32": "32", "LP64": "64"}
+        )
+        if data_model_param and data_model_param not in options:
+            options += ["--architecture", data_model_param]
 
-    def determine_result(self, returncode, returnsignal, output, is_timeout):
-        for line in output:
+        return [executable] + options + list(task.input_files_or_identifier)
+
+    def determine_result(self, run):
+        for line in run.output:
             if "VERIFICATION_SUCCESSFUL" in line:
                 return result.RESULT_TRUE_PROP
             elif "VERIFICATION_FAILED" in line:
