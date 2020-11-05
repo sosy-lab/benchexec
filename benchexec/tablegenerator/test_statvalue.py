06aa31f18ec9a5011269671bd3a266ad78fefcfa
Philipp Wendler <uni@philippwendler.de>
2020-11-05 15:25:27 +0100

    Remove use of floats from test_statvalue
    Instead we create Decimal instances from strings, which is exact.
    We can also replace most assertAlmostEqual with assertEqual.

--- benchexec/tablegenerator/test_statvalue.py
+++ benchexec/tablegenerator/test_statvalue.py
@@ -24,43 +24,43 @@ class TestStatValue(unittest.TestCase):
         self.assertIsNone(StatValue.from_list([]))
 
     def test_single_value(self):
-        v = Decimal(1.23)
+        v = Decimal("1.23")
         s = StatValue.from_list([v])
-        self.assertAlmostEqual(s.sum, v)
-        self.assertAlmostEqual(s.avg, v)
+        self.assertEqual(s.sum, v)
+        self.assertEqual(s.avg, v)
         self.assertEqual(s.max, v)
         self.assertEqual(s.min, v)
         self.assertEqual(s.median, v)
-        self.assertAlmostEqual(s.stdev, Decimal(0))
+        self.assertEqual(s.stdev, Decimal(0))
 
     def test_two_values(self):
-        v1 = Decimal(1.23)
-        v2 = Decimal(4.56)
+        v1 = Decimal("1.23")
+        v2 = Decimal("4.56")
         for t in [[v1, v2], [v2, v1]]:
             s = StatValue.from_list(t)
             self.assertEqual(s.sum, v1 + v2)
-            self.assertAlmostEqual(s.avg, (v1 + v2) / Decimal(2))
+            self.assertEqual(s.avg, (v1 + v2) / Decimal(2))
             self.assertEqual(s.max, v2)
             self.assertEqual(s.min, v1)
-            self.assertAlmostEqual(s.median, (v1 + v2) / Decimal(2))
-            self.assertAlmostEqual(s.stdev, Decimal(1.665))
+            self.assertEqual(s.median, (v1 + v2) / Decimal(2))
+            self.assertEqual(s.stdev, Decimal("1.665"))
 
     def test_three_values(self):
-        v1 = Decimal(0.123)
-        v2 = Decimal(4.56)
-        v3 = Decimal(789)
+        v1 = Decimal("0.123")
+        v2 = Decimal("4.56")
+        v3 = Decimal("789")
         for t in [[v1, v2, v3], [v3, v2, v1], [v2, v1, v3]]:
             s = StatValue.from_list(t)
             self.assertEqual(s.sum, v1 + v2 + v3)
-            self.assertAlmostEqual(s.avg, (v1 + v2 + v3) / Decimal(3))
+            self.assertEqual(s.avg, (v1 + v2 + v3) / Decimal(3))
             self.assertEqual(s.max, v3)
             self.assertEqual(s.min, v1)
             self.assertEqual(s.median, v2)
-            self.assertAlmostEqual(s.stdev, Decimal(370.83879721))
+            self.assertAlmostEqual(s.stdev, Decimal("370.83879721"))
 
     def test_nan(self):
         nan = Decimal("nan")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         s = StatValue.from_list([nan])
         self.assertTrue(s.sum.is_nan(), "Not NaN, but " + str(s.sum))
@@ -80,7 +80,7 @@ class TestStatValue(unittest.TestCase):
 
     def test_one_inf(self):
         inf = Decimal("inf")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         s = StatValue.from_list([inf])
         self.assertEqual(s.sum, inf, "Not Inf, but " + str(s.sum))
@@ -101,7 +101,7 @@ class TestStatValue(unittest.TestCase):
     def test_one_negative_inf(self):
         ninf = Decimal("-inf")
         inf = Decimal("inf")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         s = StatValue.from_list([ninf])
         self.assertEqual(s.sum, ninf, "Not -Inf, but " + str(s.sum))
@@ -121,7 +121,7 @@ class TestStatValue(unittest.TestCase):
 
     def test_multiple_positive_inf(self):
         inf = Decimal("inf")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         # Equal number of infs
         s = StatValue.from_list([inf, inf, v])
@@ -144,7 +144,7 @@ class TestStatValue(unittest.TestCase):
     def test_multiple_negative_inf(self):
         ninf = Decimal("-inf")
         inf = Decimal("inf")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         # Equal number of negative infs
         s = StatValue.from_list([ninf, ninf, v])
@@ -167,7 +167,7 @@ class TestStatValue(unittest.TestCase):
     def test_multiple_positive_and_negative_inf(self):
         inf = Decimal("inf")
         ninf = Decimal("-inf")
-        v = Decimal(0.123)
+        v = Decimal("0.123")
 
         s = StatValue.from_list([inf, ninf, v])
         self.assertTrue(s.sum.is_nan(), "Not NaN, but " + str(s.sum))
