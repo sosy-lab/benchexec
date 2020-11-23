0000c0e00eb66ae7d8b53266e57f842324c6b685
Philipp Wendler <uni@philippwendler.de>
2020-11-23 18:52:19 +0100

    Don't use logging.error for warning messages.
    We use a consistent logging scheme:
    error is for cases where we have to abort,
    warning is for cases where we continue.
    This was the only place that used logging.error for a warning in the
    table-generator.

--- benchexec/tablegenerator/columns.py
+++ benchexec/tablegenerator/columns.py
@@ -281,7 +281,7 @@ class Column(object):
             else:
                 self.type = result
         except util.TableDefinitionError as e:
-            logging.error("Column type couldn't be determined: %s", e)
+            logging.warning("Column type couldn't be determined: %s", e)
             self.type = ColumnType.text
 
         if not self.is_numeric():
