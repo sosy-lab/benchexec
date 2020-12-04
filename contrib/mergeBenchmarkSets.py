64b8b8be77b91dbd25f5cdf47564a739dbdb9da6
Marek Chalupa <mchqwerty@gmail.com>
2020-12-04 16:01:15 +0100

    contrib/mergeBenchmarkSets.py: make correct + invalid witness file as correct-unconfirmed
    Following the discussion in
    https://gitlab.com/sosy-lab/sv-comp/archives-2021/-/issues/23#note_460576708

--- contrib/mergeBenchmarkSets.py
+++ contrib/mergeBenchmarkSets.py
@@ -69,12 +69,7 @@ def getWitnessResult(witness, verification_result):
     # then leave status and category as is.
     if status_from_validation == status_from_verification:
         return status_from_verification, category_from_verification
-    # An invalid witness counts as error of the verifier.
-    if status_from_validation == "ERROR (invalid witness syntax)":
-        return (
-            "witness invalid (" + status_from_verification + ")",
-            result.CATEGORY_ERROR,
-        )
+
     # Other unconfirmed witnesses count as CATEGORY_CORRECT_UNCONFIRMED.
     if category_from_verification == result.CATEGORY_CORRECT:
         return status_from_verification, result.CATEGORY_CORRECT_UNCONFIRMED
