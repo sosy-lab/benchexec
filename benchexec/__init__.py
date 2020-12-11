b278ebbb35977d3761df0dea66c80b0d4f43300e
Philipp Wendler <uni@philippwendler.de>
2020-12-11 16:02:28 +0100

    Release 3.6

--- benchexec/__init__.py
+++ benchexec/__init__.py
@@ -34,7 +34,7 @@ Variables ending with "file" contain filenames.
 Variables ending with "tag" contain references to XML tag objects created by the XML parser.
 """
 
-__version__ = "3.6-dev"
+__version__ = "3.6"
 
 
 class BenchExecException(Exception):
