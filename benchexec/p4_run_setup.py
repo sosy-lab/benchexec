0274f70beb766111f79d8f630fa93bf3ef75e8b8
Johan Paulsson <johan.paulsson@combitech.se>
2021-02-15 03:52:25 -0800

    Updated p4 setup to forward expected results

--- benchexec/p4_run_setup.py
+++ benchexec/p4_run_setup.py
@@ -8,13 +8,18 @@ class P4SetupHandler(object):
 
     def update_runsets(self):
         for runSet in self.benchmark.run_sets:
+            runset_temp = runSet.runs.copy()
+            expected_result = runset_temp[0].expected_results
+            properties = runset_temp[0].properties
             runSet.runs = []
             for module_name in self.test_dict:
                 for test_name in self.test_dict[module_name]:
                     run = Run(module_name + "." + test_name,
                     "Fake",
-                    None,
-                    None,
-                    runSet)
+                    "",
+                    "",
+                    runSet,
+                    expected_results=expected_result)
+                    run.properties = properties
                     runSet.runs.append(run)
 
