ddf19aed940c5eb495d2f0f6b1f5a82513009108
Dennis Simon <dennism.simon94@gmail.com>
2021-02-17 19:58:55 +0100

    Revert "remove render pass by replacing react-tables onChange handler with custom implementation"
    This reverts commit 007dc48a9be7b1f499574fe414362f08c853c553.

--- benchexec/tablegenerator/react-table/src/components/ReactTable.js
+++ benchexec/tablegenerator/react-table/src/components/ReactTable.js
@@ -5,14 +5,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import React, {
-  useState,
-  useEffect,
-  memo,
-  useMemo,
-  useCallback,
-  useRef,
-} from "react";
+import React, { useState, useEffect, memo, useMemo, useCallback } from "react";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import withFixedColumns from "react-table-hoc-fixed-columns";
@@ -63,83 +56,12 @@ const initialPageSize = 250;
 const getPageSizeFromURL = () =>
   parseInt(getHashSearch().pageSize) || initialPageSize;
 
-const debounceTime = 500;
 const TableRender = (props) => {
   const [fixed, setFixed] = useState(true);
   let [filteredColumnValues, setFilteredColumnValues] = useState({});
   let [disableTaskText, setDisableTaskText] = useState(false);
   let [sortingSettings, setSortingSettings] = useState();
   let [pageSize, setPageSize] = useState(initialPageSize);
-
-  const onFilterChanged = (key, filterValue) => {
-    let updatedOldValue = false;
-    let filtered = props.filtered.map((filter) => {
-      if (filter.id === key) {
-        updatedOldValue = true;
-        return { id: key, value: filterValue };
-      }
-      return filter;
-    });
-    if (!updatedOldValue) {
-      filtered.push({ id: key, value: filterValue });
-    }
-    /* There may be filters without values left over when the filter tab
-         overrides the table tab filters. Remove those if any exist. */
-    filtered = filtered.filter((filter) => filter.value);
-    // We only want to consider filters that were set by ReactTable on this update
-    const newFilters = filtered.filter(
-      (filter) => !props.filtered.includes(filter),
-    );
-
-    filtered
-      .filter((filter) => filter.id === "id")
-      .forEach((filter) => {
-        filter.isTableTabFilter = true;
-      });
-
-    let filteredCopy = [...filtered];
-
-    let additionalFilters = [];
-
-    // We are only interested in applying additional filters based on status filters
-    const statusFilter = newFilters.filter(({ id, value }) =>
-      id.includes("status"),
-    );
-    if (statusFilter && statusFilter.length) {
-      const parsed = statusFilter.map(({ id, value }) => {
-        const [tool, name, column] = id.split("_");
-        return {
-          tool,
-          name,
-          column,
-          value,
-        };
-      });
-
-      for (const { tool, name, column, value } of parsed) {
-        if (value.trim() === "all") {
-          additionalFilters = selectAllStatusFields({
-            tool,
-            name,
-            column,
-          });
-          filteredCopy = filteredCopy.filter(
-            ({ id, value }) =>
-              !(id === `${tool}_${name}_${column}` && value.trim() === "all"),
-          );
-        } else {
-          const isCategory = value[value.length - 1] === " ";
-          additionalFilters = createAdditionalFilters({
-            tool,
-            name,
-            column,
-            isCategory,
-          });
-        }
-      }
-    }
-    props.filterPlotData([...filteredCopy, ...additionalFilters], true);
-  };
 
   function FilterInputField(props) {
     const elementId = props.column.id + "_filter";
@@ -156,9 +78,9 @@ const TableRender = (props) => {
       value = event.target.value;
       clearTimeout(typingTimer);
       typingTimer = setTimeout(() => {
-        onFilterChanged(props.column.id, value);
+        props.onChange(value);
         document.getElementById(elementId).focus();
-      }, debounceTime);
+      }, 500);
     };
 
     return (
@@ -354,7 +276,7 @@ const TableRender = (props) => {
       filterMethod: (filter, row) => {
         return true;
       },
-      Filter: ({ filter, onChange, column }) => {
+      Filter: ({ filter, onChange }) => {
         const categoryValues = props.categoryValues[runSetIdx][columnIdx];
         const selectedCategoryFilters = pathOr(
           [runSetIdx, "categories"],
@@ -382,7 +304,7 @@ const TableRender = (props) => {
         const selectValue = multipleSelected ? "multiple" : singleFilterValue;
         return (
           <select
-            onChange={(event) => onFilterChanged(column.id, event.target.value)}
+            onChange={(event) => onChange(event.target.value)}
             style={{ width: "100%" }}
             value={
               (allSelected && "all ") ||
@@ -522,6 +444,8 @@ const TableRender = (props) => {
     return out;
   };
 
+  const mTable = useCallback((_, makeTable) => makeTable(), [props.data]);
+
   console.log({ filtered: props.filtered });
 
   return (
@@ -545,8 +469,71 @@ const TableRender = (props) => {
         pageSizeOptions={[50, 100, 250, 500, 1000, 2500]}
         className="-highlight"
         minRows={0}
+        onFilteredChange={(filtered) => {
+          /* There may be filters without values left over when the filter tab
+             overrides the table tab filters. Remove those if any exist. */
+          filtered = filtered.filter((filter) => filter.value);
+          // We only want to consider filters that were set by ReactTable on this update
+          const newFilters = filtered.filter(
+            (filter) => !props.filtered.includes(filter),
+          );
+
+          filtered
+            .filter((filter) => filter.id === "id")
+            .forEach((filter) => {
+              filter.isTableTabFilter = true;
+            });
+
+          let filteredCopy = [...filtered];
+
+          let additionalFilters = [];
+
+          // We are only interested in applying additional filters based on status filters
+          const statusFilter = newFilters.filter(({ id, value }) =>
+            id.includes("status"),
+          );
+          if (statusFilter && statusFilter.length) {
+            const parsed = statusFilter.map(({ id, value }) => {
+              const [tool, name, column] = id.split("_");
+              return {
+                tool,
+                name,
+                column,
+                value,
+              };
+            });
+
+            for (const { tool, name, column, value } of parsed) {
+              if (value.trim() === "all") {
+                additionalFilters = selectAllStatusFields({
+                  tool,
+                  name,
+                  column,
+                });
+                filteredCopy = filteredCopy.filter(
+                  ({ id, value }) =>
+                    !(
+                      id === `${tool}_${name}_${column}` &&
+                      value.trim() === "all"
+                    ),
+                );
+              } else {
+                const isCategory = value[value.length - 1] === " ";
+                additionalFilters = createAdditionalFilters({
+                  tool,
+                  name,
+                  column,
+                  isCategory,
+                });
+              }
+            }
+          }
+          props.filterPlotData([...filteredCopy, ...additionalFilters], true);
+        }}
         onPageSizeChange={(pageSize) => setParam({ pageSize })}
-      />
+      >
+        {mTable}
+      </ReactTableFixedColumns>
     </div>
   );
 };
