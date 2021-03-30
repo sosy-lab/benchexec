65dc3dba5328040cbfecfdb80b264c0947f10444
Johan Paulsson <johan.paulsson@combitech.se>
2021-03-30 00:27:43 -0700

    Added test to test more complex example test setup

--- contrib/p4/docker_files/ptf_tester/tests/test_no_fail.py
+++ contrib/p4/docker_files/ptf_tester/tests/test_no_fail.py
@@ -5,6 +5,19 @@ import ptf.testutils as testutils
 from scapy.all import *
 import re
 
+TYPE_MYTUNNEL = 0x1212
+TYPE_IPV4 = 0x0800
+
+
+class MyTunnel(Packet):
+    name="MyTunnel"
+    fields_desc = [
+        ShortField("pid", 0),
+        ShortField("dst_id", 0)
+    ]
+    def mysummary(self):
+        return self.sprintf("pid=%pid%, dst_id=%dst_id%")
+
 class DataplaneBaseTest(BaseTest):
     def __init__(self):
         BaseTest.__init__(self)
@@ -20,53 +33,55 @@ class DataplaneBaseTest(BaseTest):
         if config["log_dir"] != None:
             self.dataplane.stop_pcap()
 
-class OneTest(DataplaneBaseTest):
+class IPV4OneSwitchTest(DataplaneBaseTest):
     def __init__(self):
         DataplaneBaseTest.__init__(self)
 
     def runTest(self):
         #log_to_file("Staring first test")
-        pkt = testutils.simple_tcp_packet(pktlen=100)
-        pkt_failed = testutils.simple_tcp_packet(pktlen=11)
-
+        pkt = testutils.simple_tcp_packet(pktlen=100, ip_dst="192.168.1.2", ip_ttl=64, eth_dst="00:01:02:03:04:05", eth_src="00:01:02:03:04:05")
+        pkt_expected = testutils.simple_tcp_packet(pktlen=100, ip_dst="192.168.1.2", ip_ttl=63, eth_dst="00:01:02:03:04:05", eth_src="00:01:02:03:04:05")
         try:
             testutils.send_packet(self, (0, 1), pkt)
         except Exception as e:
             print(e)
         
         
-        testutils.verify_packet(self, pkt, (1, 1))
+        testutils.verify_packet(self, pkt_expected, (1, 1))
 
-class TwoTest(DataplaneBaseTest):
+class Ipv4TwoSwitchesTest(DataplaneBaseTest):
     def __init__(self):
         DataplaneBaseTest.__init__(self)
 
     def runTest(self):
-        #log_to_file("Starting second test")
-        pkt = testutils.simple_tcp_packet(pktlen=100)
-        pkt_failed = testutils.simple_tcp_packet(pktlen=101)
+        pkt = testutils.simple_tcp_packet(pktlen=100, ip_dst="192.168.1.4", ip_ttl=64, eth_dst="00:01:02:03:04:05", eth_src="00:01:02:03:04:05")
+        pkt_expected = testutils.simple_tcp_packet(pktlen=100, ip_dst="192.168.1.4", ip_ttl=62, eth_dst="00:01:02:03:04:05", eth_src="00:01:02:03:04:05")
 
-        testutils.send_packet(self, (0,1), pkt)
+        testutils.send_packet(self, (0, 1), pkt)
 
-        testutils.verify_packet(self, pkt, (1, 1))
+        testutils.verify_packet(self, pkt_expected, (3,1))
+# class MyTunnelPacketTest(DataplaneBaseTest):
+#     def __init__(self):
+#         DataplaneBaseTest.__init__(self)
 
-class ThreeTest(DataplaneBaseTest):
-    def __init__(self):
-        DataplaneBaseTest.__init__(self)
+#     def runTest(self):
+#         #log_to_file("Starting second test")
 
-    def runTest(self):
-        #log_to_file("Staring Third test")
-        pkt = testutils.simple_tcp_packet(pktlen=200)
+#         bind_layers(Ether, MyTunnel, type=TYPE_MYTUNNEL)
+#         bind_layers(MyTunnel, IP, pid=TYPE_IPV4)
+#         pkt = Ether(dst="00:01:02:03:04:05", src="00:01:02:03:04:05")/MyTunnel()/IP(dst="192.168.1.2", ttl=64)
 
-        try:
-            testutils.send_packet(self, (0, 1), pkt)
-        except Exception as e:
-            log_to_file(e)
-        
-        testutils.verify_packet(self, pkt, (1, 1))
+#         log_to_file("---Packet--- \n" + str(pkt.show))
+
+#         pkt_expected = Ether(dst="00:01:02:03:04:05", src="00:01:02:03:04:05")/IP(dst="192.168.1.2", ttl=63)
+
+#         testutils.send_packet(self, (0,1), pkt)
+
+#         testutils.verify_packet(self, pkt_expected, (1, 1))
+
 
 def log_to_file(msg):
-    f = open("/app/app.log", "a")
+    f = open("/app/app.log", "w")
     f.write(msg + "\n")
     f.close()
 
