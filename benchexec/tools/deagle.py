50e1e8d6fa24b0cbf7330411ce4d87b9cb351f54
Sun Zhihang <841091517@qq.com>
2021-10-09 16:57:24 +0800

    Add a tool info module for Deagle

---
+++ benchexec/tools/deagle.py
@@ -0,0 +1,43 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import benchexec.result as result
+import benchexec.util as util
+import benchexec.tools.template
+
+
+class Tool(benchexec.tools.template.BaseTool):
+    def executable(self):
+        return util.find_executable("deagle")
+
+    def name(self):
+        return "Deagle"
+
+    def version(self, executable):
+        return self._version_from_tool(executable)
+
+    def cmdline(self, executable, options, tasks, propertyfile, rlimits):
+        options = options + ["--32", "--no-unwinding-assertions", "--closure"]
+        return [executable] + options + tasks
+
+    def determine_result(self, returncode, returnsignal, output, isTimeout):
+
+        status = result.RESULT_UNKNOWN
+        stroutput = str(output)
+
+        if isTimeout:
+            status = "TIMEOUT"
+        elif "SUCCESSFUL" in stroutput:
+            status = result.RESULT_TRUE_PROP
+        elif "FAILED" in stroutput:
+            status = result.RESULT_FALSE_REACH
+        elif "UNKNOWN" in stroutput:
+            status = result.RESULT_UNKNOWN
+        else:
+            status = result.RESULT_UNKNOWN
+
+        return status
