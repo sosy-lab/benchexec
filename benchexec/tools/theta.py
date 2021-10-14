f68e24b4997fc5f41a2a3559a54e5555b194b7a7
AdamZsofi <adamzsofi.mail@gmail.com>
2021-10-14 10:26:34 +0200

    Added suggested changes to Theta tool info

--- benchexec/tools/theta.py
+++ benchexec/tools/theta.py
@@ -1,3 +1,9 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
 import benchexec.result as result
 import benchexec.tools.template
 
@@ -5,26 +11,27 @@ import benchexec.tools.template
 class Tool(benchexec.tools.template.BaseTool2):
     """
     Theta
-    A generic, modular and configurable model checking framework developed at the Critical Systems Research Group of Budapest University of Technology and Economics, aiming to support the design and evaluation of abstraction refinement-based algorithms for the reachability analysis of various formalisms.
+    A generic, modular and configurable model checking framework developed
+    at the Critical Systems Research Group of Budapest University
+    of Technology and Economics, aiming to support the design and
+    evaluation of abstraction refinement-based algorithms for the
+    reachability analysis of various formalisms.
 
     https://github.com/ftsrg/theta
     """
 
-    REQUIRED_PATHS = [".."]
-
     def executable(self, tool_locator):
-        return tool_locator.find_executable("theta-start.sh", subdir="")
+        return tool_locator.find_executable("theta-start.sh")
 
     def name(self):
-        return "theta"
+        return "Theta"
 
     def version(self, executable):
         return self._version_from_tool(executable)
 
     def cmdline(self, executable, options, task, rlimits):
-        return (
-            [executable] + [task.single_input_file] + options
-        )  # we only support unreach-call
+        # we only support unreach-call and ignore property file
+        return [executable, task.single_input_file] + options
 
     def determine_result(self, run):
         status = result.RESULT_UNKNOWN
