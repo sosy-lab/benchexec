74e9e4b7fa05c54856f7e71e547f7aff832c3043
kaled Alshmrany <kaled.alshmrany@postgrad.manchester.ac.uk>
2021-11-01 18:42:29 +0300

    Update fusebmc.py

--- benchexec/tools/fusebmc.py
+++ benchexec/tools/fusebmc.py
@@ -15,40 +15,19 @@ class Tool(benchexec.tools.template.BaseTool2):
     This class serves as tool adaptor for FuSeBMC (https://github.com/kaled-alshmrany/FuSeBMC)
     """
 
-    REQUIRED_PATHS_TESTCOMP20 = ["esbmc", "esbmc-wrapper.py", "my_instrument"]
-    REQUIRED_PATHS_TESTCOMP21 = [
-        "esbmc",
-        "fusebmc.py",
-        "FuSeBMC_inustrment/FuSeBMC_inustrment",
-        "fusebmc_output",
-        "map2check-fuzzer",
-    ]
-    REQUIRED_PATHS_TESTCOMP22 = [
-        "FuSeBMC_engines",
-        "FuSeBMC_FuzzerLib",
-        "FuSeBMC_GoalTracerLib",
-        "FuSeBMC_inustrument/FuSeBMC_inustrment",
-        "fusebmc_output",
-        "FuSeBMC_SeedGenLib",
-        "FuSeBMC_TCGen",
-        "fusebmc_util",
-        "fusebmc.py",
-        "fusebmc_getinput_from_testcase.py",
-        "fusebmc_seed_gen.py",
-    ]
     def executable(self, tool_locator):
         return tool_locator.find_executable("fusebmc.py")
-    
+
     def working_directory(self, executable):
         executableDir = os.path.dirname(executable)
         return executableDir
-    
+
     def version(self, executable):
         return self._version_from_tool(executable, "-v")
-    
+
     def name(self):
         return "FuSeBMC"
-    
+
     def cmdline(self, executable, options, task, rlimits):
         data_model_param = get_data_model_from_task(task, {ILP32: "32", LP64: "64"})
         if data_model_param and "--arch" not in options:
@@ -59,27 +38,17 @@ class Tool(benchexec.tools.template.BaseTool2):
             + options
             + [task.single_input_file]
         )
-    
-    def program_files(self, executable):
-        """
-        Determine the file paths to be adopted
-        """
-        if self._version == 21:
-            paths = self.REQUIRED_PATHS_TESTCOMP21
-        elif self._version > 21:
-            paths = self.REQUIRED_PATHS_TESTCOMP22
-        return self._program_files_from_executable(executable, paths)
-    
+
     def determine_result(self, run):
         status = result.RESULT_UNKNOWN
         
         if run.output.any_line_contains("DONE"):
             status = result.RESULT_DONE
-            
+
         if status == result.RESULT_UNKNOWN:
             if run.was_timeout:
                 status = "TIMEOUT"
             elif not run.output.any_line_contains("Unknown"):
                 status = "ERROR"
-                
+
         return status
