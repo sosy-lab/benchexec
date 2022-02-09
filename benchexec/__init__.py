d17013a06d44c910ffa813899f02bae97496801e
Philipp Wendler <uni@philippwendler.de>
2022-02-09 13:46:43 +0100

    Prepare version number for next development cycle.

--- benchexec/__init__.py
+++ benchexec/__init__.py
@@ -34,7 +34,7 @@ Variables ending with "file" contain filenames.
 Variables ending with "tag" contain references to XML tag objects created by the XML parser.
 """
 
-__version__ = "3.11"
+__version__ = "3.12-dev"
 
 
 class BenchExecException(Exception):  # noqa: N818 backwards compatibility
