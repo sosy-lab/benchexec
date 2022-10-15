e19202138dc2d87166711e7edf212c03d26286c3
Lapulatos <2627231235@qq.com>
2022-10-15 13:03:14 +0800

    Add PIChecker tool-info

---
+++ benchexec/tools/pichecker.py
@@ -0,0 +1,23 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import benchexec.tools.cpachecker as cpachecker
+
+
+class Tool(cpachecker.Tool):
+    """
+    Tool info for PIChecker.
+    URL: https://gitlab.com/Lapulatos/pichecker
+    """
+
+    REQUIRED_PATHS = list(cpachecker.Tool.REQUIRED_PATHS) + ["resources"]
+
+    def version(self, executable):
+        return "1.0"
+
+    def name(self):
+        return "PIChecker"
