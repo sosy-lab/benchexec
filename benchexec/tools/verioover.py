86e1e3d96a57571fd77e9f02a67fc5c3ac04a128
Mete0r <61377699+Mete0o0r@users.noreply.github.com>
2022-10-21 15:15:03 +0800

    Update verioover.py

--- benchexec/tools/verioover.py
+++ benchexec/tools/verioover.py
@@ -1,6 +1,11 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+# SPDX-FileCopyrightText: 2016-2020 Marek Chalupa
+#
+# SPDX-License-Identifier: Apache-2.0
 
-
-import benchexec.util as util
 import benchexec.tools.template
 import benchexec.result as result
 
@@ -8,6 +13,8 @@ import benchexec.result as result
 class Tool(benchexec.tools.template.BaseTool2):
     """
     VeriOover
+
+    http://github.com/PaperSheeper/VeriOover-SV
     """
 
     def name(self):
@@ -15,30 +22,26 @@ class Tool(benchexec.tools.template.BaseTool2):
 
     def executable(self, tool_locator):
         return tool_locator.find_executable("VeriOover")
-    
+
     def version(self, executable):
         return self._version_from_tool(executable)
 
     def cmdline(self, executable, options, task, rlimits):
-        if "-file" not in options:
-            options = options + ["-file"]
-
-        options = options + list(task.input_files_or_identifier)
+        if task.single_input_file:
+            options = options + ["-file", task.single_input_file]
 
         if task.property_file:
             options = options + ["-spec", task.property_file]
 
         return [executable] + options
 
-
     def determine_result(self, run):
         # parse output
         status = result.RESULT_UNKNOWN
-        print(run.output)
         for line in run.output:
             if "spec incorrect!" in line:
                 status = result.RESULT_FALSE_PROP
-            elif "spec unknow!" in line:
+            elif "spec unknown!" in line:
                 status = result.RESULT_UNKNOWN
             elif "spec correct!" in line:
                 status = result.RESULT_TRUE_PROP
