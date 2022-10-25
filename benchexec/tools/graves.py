1dfbac10d0ef66b694883570fd95cff0a70a910a
will-leeson <wel2vw@virginia.edu>
2022-10-25 12:37:58 -0400

    Updating Graves-CPA and Graves-Par files

--- benchexec/tools/graves.py
+++ benchexec/tools/graves.py
@@ -5,74 +5,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import benchexec.tools.coveriteam as coveriteam
-import benchexec.result as result
-from benchexec.tools.template import UnsupportedFeatureException
-from benchexec.tools.sv_benchmarks_util import get_data_model_from_task, ILP32, LP64
-import re
+import benchexec.tools.cpachecker as cpachecker
 
-class Tool(coveriteam.Tool):
+
+class Tool(cpachecker.Tool):
     """
-    Tool infor for Graves, a verifier selector based on Graph Neural Networks 
-    We inherit Coveriteam's infrastructure. 
+    Tool info for Graves-CPA.
+    URL: https://github.com/will-leeson/cpachecker
     """
 
-    REQUIRED_PATHS = [
-        "coveriteam",
-        "bin",
-        "lib",
-        "depends",
-        "predict",
-        "predict/build",
-    ]
+    REQUIRED_PATHS = list(cpachecker.Tool.REQUIRED_PATHS) + ["resources"]
 
     def name(self):
-        return "Graves"
-
-    def executable(self, tool_locator):
-        return tool_locator.find_executable("graves.sh")
-
-    def version(self, executable):
-        return self._version_from_tool(executable)
-
-    def program_files(self, executable):
-        return self._program_files_from_executable(
-            executable, self.REQUIRED_PATHS, parent_dir=True
-        )
-
-    def cmdline(self, executable, options, task, rlimits):
-        """
-        Modified from the CoVeriTeam definition
-        """
-
-        if task.property_file:
-            options += [task.property_file]
-        else:
-            raise UnsupportedFeatureException(
-                "Can't execute Graves: "
-                "Specification is missing."
-            )
-
-        options += [task.single_input_file]
-
-        return [executable] + options
-
-    def determine_result(self, run):
-        """
-        It assumes that any verifier or validator implemented in CoVeriTeam
-        will print out the produced aftifacts.
-        If more than one dict is printed, the first matching one.
-        """
-        verdict = None
-        verdict_regex = re.compile(r"'verdict': '([a-zA-Z\(\)\ \-]*)'")
-        for line in reversed(run.output):
-            line = line.strip()
-            verdict_match = verdict_regex.search(line)
-            if verdict_match and verdict is None:
-                # CoVeriTeam outputs benchexec result categories as verdicts.
-                verdict = verdict_match.group(1)
-            if "Traceback (most recent call last)" in line:
-                verdict = "EXCEPTION"
-        if verdict is None:
-            return result.RESULT_UNKNOWN
-        return verdict
+        return "Graves-CPA"
