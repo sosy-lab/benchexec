1152a0c79d5ec08d8297e1d10b73d65fb8ac05b9
Philipp Wendler <uni@philippwendler.de>
2022-11-22 18:09:06 +0100

    code format

--- benchexec/tools/wasp-c.py
+++ benchexec/tools/wasp-c.py
@@ -10,6 +10,7 @@ import benchexec.result as result
 
 from benchexec.tools.sv_benchmarks_util import get_data_model_from_task, ILP32, LP64
 
+
 class Tool(benchexec.tools.template.BaseTool2):
     """
     This class serves as tool adaptor for WASP-C (https://github.com/wasp-platform/wasp)
@@ -34,7 +35,6 @@ class Tool(benchexec.tools.template.BaseTool2):
             )
 
         data_model_param = get_data_model_from_task(task, {ILP32: "32", LP64: "64"})
-#
         if data_model_param and "--arch" not in options:
             options += ["--arch", data_model_param]
 
