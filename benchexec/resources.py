bb281b07ed1df19b36af62381dcf65e4ce5a9981
CGall42 <charlotte.wegmann@campus.lmu.de>
2023-02-07 10:38:34 +0100

    Fixed copyright header

--- benchexec/resources.py
+++ benchexec/resources.py
@@ -1,4 +1,9 @@
-# resources_new.py
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
 
 """
 This module contains functions for computing assignments of resources to runs.
