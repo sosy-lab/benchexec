34cdb5f2ca474bc127c55bd318cd5db9c73fbca4
Philipp Wendler <uni@philippwendler.de>
2023-04-03 12:01:11 +0200

    Refactoring: move functionality into write_file() where it is reusable

--- benchexec/cgroups.py
+++ benchexec/cgroups.py
@@ -495,18 +495,13 @@ class Cgroup(object):
                 yield from recursive_child_cgroups(cgroup)
 
         def try_unfreeze(cgroup):
-            freezer_file = os.path.join(cgroup, "freezer.state")
             try:
-                util.write_file("THAWED", freezer_file)
+                util.write_file("THAWED", cgroup, "freezer.state", force=True)
             except OSError:
-                # Somebody could have fiddle with permissions, try to set them.
-                # If we are not owner, this also fails, but then there is nothing we
-                # can do. But the processes inside the run cannot change the owner.
-                try:
-                    os.chmod(freezer_file, stat.S_IRUSR | stat.S_IWUSR)
-                    util.write_file("THAWED", freezer_file)
-                except OSError:
-                    pass
+                # With force=True this fails only if we are not owner, but then there is
+                # nothing we can do. But the processes inside the run cannot change the
+                # owner, so this should not happen.
+                pass
 
         # First, we go through all cgroups recursively while they are frozen and kill
         # all processes. This helps against fork bombs and prevents processes from
