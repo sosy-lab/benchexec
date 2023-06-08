11994cc9217b2fb74bee3bb9e45eadbf9e501d7f
Naman-Priyadarshi <namanpriyadarshicr7@gmail.com>
2023-06-08 13:24:15 +0530

    migrate test_benchmark_definiton.py

--- benchexec/test_benchmark_definition.py
+++ benchexec/test_benchmark_definition.py
@@ -8,7 +8,7 @@
 import collections
 import os
 import tempfile
-import unittest
+import pytest
 from unittest.mock import patch
 import yaml
 
@@ -63,97 +63,103 @@ def mock_property_create(property_file):
     return benchexec.result.Property("test.prp", False, "test")
 
 
-class TestBenchmarkDefinition(unittest.TestCase):
-    """
-    Unit tests for reading benchmark definitions,
-    testing mostly the classes from benchexec.model.
-    """
+@pytest.fixture(autouse=True)
+def mock_benchmark_fixture(request):
+    patchers = [
+        patch("benchexec.model.load_task_definition_file", new=mock_load_task_def_file),
+        patch("benchexec.result.Property.create", new=mock_property_create),
+        patch("benchexec.util.expand_filename_pattern", new=mock_expand_filename_pattern),
+        patch("os.path.samefile", new=lambda a, b: a == b)
+    ]
+    for patcher in patchers:
+        patcher.start()
+        request.addfinalizer(patcher.stop)
 
-    @classmethod
-    def setUpClass(cls):
-        cls.longMessage = True
 
-    @patch("benchexec.model.load_task_definition_file", new=mock_load_task_def_file)
-    @patch("benchexec.result.Property.create", new=mock_property_create)
-    @patch("benchexec.util.expand_filename_pattern", new=mock_expand_filename_pattern)
-    @patch("os.path.samefile", new=lambda a, b: a == b)
-    def parse_benchmark_definition(self, content):
-        with tempfile.NamedTemporaryFile(
-            prefix="BenchExec_test_benchmark_definition_", suffix=".xml", mode="w+"
-        ) as temp:
-            temp.write(content)
-            temp.flush()
+def parse_benchmark_definition(content):
+    with tempfile.NamedTemporaryFile(
+        prefix="BenchExec_test_benchmark_definition_", suffix=".xml", mode="w+"
+    ) as temp:
+        temp.write(content)
+        temp.flush()
 
-            # Because we mocked everything that accesses the file system,
-            # we can parse the benchmark definition although task files do not exist.
-            return Benchmark(temp.name, DummyConfig, util.read_local_time())
+        # Because we mocked everything that accesses the file system,
+        # we can parse the benchmark definition although task files do not exist.
+        return Benchmark(temp.name, DummyConfig, util.read_local_time())
 
-    def check_task_filter(self, filter_attr, expected):
-        # The following three benchmark definitions are equivalent, we check each.
-        benchmark_definitions = [
-            """
-            <benchmark tool="dummy">
-              <propertyfile {}>test.prp</propertyfile>
-              <tasks><include>*.yml</include></tasks>
-              <rundefinition/>
-            </benchmark>
-            """,
-            """
-            <benchmark tool="dummy">
-              <tasks>
-                <propertyfile {}>test.prp</propertyfile>
-                <include>*.yml</include>
-              </tasks>
-              <rundefinition/>
-            </benchmark>
-            """,
-            """
-            <benchmark tool="dummy">
-              <tasks>
-                <include>*.yml</include>
-              </tasks>
-              <rundefinition>
-                <propertyfile {}>test.prp</propertyfile>
-              </rundefinition>
-            </benchmark>
-            """,
-        ]
 
-        for bench_def in benchmark_definitions:
-            benchmark = self.parse_benchmark_definition(bench_def.format(filter_attr))
-            run_ids = [run.identifier for run in benchmark.run_sets[0].runs]
-            self.assertListEqual(run_ids, sorted(expected))
+def check_task_filter(filter_attr, expected):
+    # The following three benchmark definitions are equivalent, we check each.
+    benchmark_definitions = [
+        """
+        <benchmark tool="dummy">
+          <propertyfile {}>test.prp</propertyfile>
+          <tasks><include>*.yml</include></tasks>
+          <rundefinition/>
+        </benchmark>
+        """,
+        """
+        <benchmark tool="dummy">
+          <tasks>
+            <propertyfile {}>test.prp</propertyfile>
+            <include>*.yml</include>
+          </tasks>
+          <rundefinition/>
+        </benchmark>
+        """,
+        """
+        <benchmark tool="dummy">
+          <tasks>
+            <include>*.yml</include>
+          </tasks>
+          <rundefinition>
+            <propertyfile {}>test.prp</propertyfile>
+          </rundefinition>
+        </benchmark>
+        """,
+    ]
 
-    def test_expected_verdict_no_filter(self):
-        self.check_task_filter("", ALL_TEST_TASKS.keys())
-
-    def test_expected_verdict_true_filter(self):
-        self.check_task_filter('expectedverdict="true"', ["true_task.yml"])
-
-    def test_expected_verdict_false_filter(self):
-        false_tasks = [f for f in ALL_TEST_TASKS.keys() if f.startswith("false")]
-        self.check_task_filter('expectedverdict="false"', false_tasks)
-
-    def test_expected_verdict_false_subproperty_filter(self):
-        self.check_task_filter('expectedverdict="false(sub)"', ["false_sub_task.yml"])
-
-    def test_expected_verdict_unknown_filter(self):
-        self.check_task_filter('expectedverdict="unknown"', ["unknown_task.yml"])
-
-    def test_expected_verdict_false_subproperties_filter(self):
-        benchmark_definition = """
-            <benchmark tool="dummy">
-              <tasks>
-                <propertyfile expectedverdict="false(sub)">test.prp</propertyfile>
-                <include>*.yml</include>
-              </tasks>
-              <tasks>
-                <propertyfile expectedverdict="false(sub2)">test.prp</propertyfile>
-                <include>*.yml</include>
-              </tasks>
-              <rundefinition/>
-            </benchmark>
-            """
-        benchmark = self.parse_benchmark_definition(benchmark_definition)
+    for bench_def in benchmark_definitions:
+        benchmark = parse_benchmark_definition(bench_def.format(filter_attr))
         run_ids = [run.identifier for run in benchmark.run_sets[0].runs]
-        self.assertListEqual(run_ids, ["false_sub_task.yml", "false_sub2_task.yml"])
+        assert run_ids == sorted(expected)
+
+
+def test_expected_verdict_no_filter():
+    check_task_filter("", ALL_TEST_TASKS.keys())
+
+
+def test_expected_verdict_true_filter():
+    check_task_filter('expectedverdict="true"', ["true_task.yml"])
+
+
+def test_expected_verdict_false_filter():
+    false_tasks = [f for f in ALL_TEST_TASKS.keys() if f.startswith("false")]
+    check_task_filter('expectedverdict="false"', false_tasks)
+
+
+def test_expected_verdict_false_subproperty_filter():
+    check_task_filter('expectedverdict="false(sub)"', ["false_sub_task.yml"])
+
+
+def test_expected_verdict_unknown_filter():
+    check_task_filter('expectedverdict="unknown"', ["unknown_task.yml"])
+
+
+def test_expected_verdict_false_subproperties_filter():
+    benchmark_definition = """
+        <benchmark tool="dummy">
+          <tasks>
+            <propertyfile expectedverdict="false(sub)">test.prp</propertyfile>
+            <include>*.yml</include>
+          </tasks>
+          <tasks>
+            <propertyfile expectedverdict="false(sub2)">test.prp</propertyfile>
+            <include>*.yml</include>
+          </tasks>
+          <rundefinition/>
+        </benchmark>
+        """
+    benchmark = parse_benchmark_definition(benchmark_definition)
+    run_ids = [run.identifier for run in benchmark.run_sets[0].runs]
+    assert run_ids == ["false_sub_task.yml", "false_sub2_task.yml"]
