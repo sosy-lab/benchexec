f8a2768fe9ee85506a6e3e460b3e74fac931445a
Naman-Priyadarshi <namanpriyadarshicr7@gmail.com>
2023-06-14 23:30:22 +0530

    migrate test_cgroups.py

--- benchexec/test_cgroups.py
+++ benchexec/test_cgroups.py
@@ -5,64 +5,53 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
 import subprocess
-import sys
-import unittest
+import pytest
 
 from benchexec import check_cgroups
 
-sys.dont_write_bytecode = True  # prevent creation of .pyc files
 
+def execute_run_extern(*args, **kwargs):
+    try:
+        return subprocess.check_output(
+            args=["python3", "-m", "benchexec.check_cgroups"] + list(args),
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+            **kwargs,
+        )
+    except subprocess.CalledProcessError as e:
+        if e.returncode != 1:  # 1 is expected if cgroups are not available
+            print(e.output)
+            raise e
 
-class TestCheckCgroups(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.longMessage = True
-        cls.maxDiff = None
-        logging.disable(logging.CRITICAL)
+def test_extern_command():
+    execute_run_extern()
 
-    def execute_run_extern(self, *args, **kwargs):
-        try:
-            return subprocess.check_output(
-                args=["python3", "-m", "benchexec.check_cgroups"] + list(args),
-                stderr=subprocess.STDOUT,
-                universal_newlines=True,
-                **kwargs,
-            )
-        except subprocess.CalledProcessError as e:
-            if e.returncode != 1:  # 1 is expected if cgroups are not available
-                print(e.output)
-                raise e
+def test_simple():
+    try:
+        check_cgroups.main(["--no-thread"])
+    except SystemExit as e:
+        # expected if cgroups are not available
+        pytest.skip(e)
 
-    def test_extern_command(self):
-        self.execute_run_extern()
+def test_threaded():
+    try:
+        check_cgroups.main([])
+    except SystemExit as e:
+        # expected if cgroups are not available
+        pytest.skip(e)
 
-    def test_simple(self):
-        try:
-            check_cgroups.main(["--no-thread"])
-        except SystemExit as e:
-            # expected if cgroups are not available
-            self.skipTest(e)
+def test_thread_result_is_returned():
+    """
+    Test that an error raised by check_cgroup_availability is correctly
+    re-raised in the main thread by replacing this function temporarily.
+    """
+    tmp = check_cgroups.check_cgroup_availability
+    try:
+        check_cgroups.check_cgroup_availability = lambda wait: exit(1)
 
-    def test_threaded(self):
-        try:
+        with pytest.raises(SystemExit):
             check_cgroups.main([])
-        except SystemExit as e:
-            # expected if cgroups are not available
-            self.skipTest(e)
 
-    def test_thread_result_is_returned(self):
-        """
-        Test that an error raised by check_cgroup_availability is correctly
-        re-raised in the main thread by replacing this function temporarily.
-        """
-        tmp = check_cgroups.check_cgroup_availability
-        try:
-            check_cgroups.check_cgroup_availability = lambda wait: exit(1)
-
-            with self.assertRaises(SystemExit):
-                check_cgroups.main([])
-
-        finally:
-            check_cgroups.check_cgroup_availability = tmp
+    finally:
+        check_cgroups.check_cgroup_availability = tmp
