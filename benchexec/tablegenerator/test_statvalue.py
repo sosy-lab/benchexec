ab25f20393e1a1a24746f56db1a545fab7d3d1a1
Naman-Priyadarshi <namanpriyadarshicr7@gmail.com>
2023-10-17 13:13:13 +0530

    migrate test_statvalue.py and test_util.py

--- benchexec/tablegenerator/test_statvalue.py
+++ benchexec/tablegenerator/test_statvalue.py
@@ -1,49 +1,42 @@
-# This file is part of BenchExec, a framework for reliable benchmarking:
-# https://github.com/sosy-lab/benchexec
-#
-# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
-#
-# SPDX-License-Identifier: Apache-2.0
-
 from decimal import Decimal
 import sys
-import unittest
+import pytest
 
 from benchexec.tablegenerator.statistics import StatValue
 
 sys.dont_write_bytecode = True  # prevent creation of .pyc files
 
 
-class TestStatValue(unittest.TestCase):
+class TestStatValue:
     @classmethod
-    def setUpClass(cls):
-        cls.longMessage = True
-        cls.maxDiff = None
+    def setup_class(cls):
+        cls.long_message = True
+        cls.max_diff = None
 
     def test_empty(self):
-        self.assertIsNone(StatValue.from_list([]))
+        assert StatValue.from_list([]) is None
 
     def test_single_value(self):
         v = Decimal("1.23")
         s = StatValue.from_list([v])
-        self.assertEqual(s.sum, v)
-        self.assertEqual(s.avg, v)
-        self.assertEqual(s.max, v)
-        self.assertEqual(s.min, v)
-        self.assertEqual(s.median, v)
-        self.assertEqual(s.stdev, Decimal(0))
+        assert s.sum == v
+        assert s.avg == v
+        assert s.max == v
+        assert s.min == v
+        assert s.median == v
+        assert s.stdev == Decimal(0)
 
     def test_two_values(self):
         v1 = Decimal("1.23")
         v2 = Decimal("4.56")
         for t in [[v1, v2], [v2, v1]]:
             s = StatValue.from_list(t)
-            self.assertEqual(s.sum, v1 + v2)
-            self.assertEqual(s.avg, (v1 + v2) / Decimal(2))
-            self.assertEqual(s.max, v2)
-            self.assertEqual(s.min, v1)
-            self.assertEqual(s.median, (v1 + v2) / Decimal(2))
-            self.assertEqual(s.stdev, Decimal("1.665"))
+            assert s.sum == v1 + v2
+            assert s.avg == (v1 + v2) / Decimal(2)
+            assert s.max == v2
+            assert s.min == v1
+            assert s.median == (v1 + v2) / Decimal(2)
+            assert s.stdev == Decimal("1.665")
 
     def test_three_values(self):
         v1 = Decimal("0.123")
@@ -51,52 +44,52 @@ class TestStatValue(unittest.TestCase):
         v3 = Decimal("789")
         for t in [[v1, v2, v3], [v3, v2, v1], [v2, v1, v3]]:
             s = StatValue.from_list(t)
-            self.assertEqual(s.sum, v1 + v2 + v3)
-            self.assertEqual(s.avg, (v1 + v2 + v3) / Decimal(3))
-            self.assertEqual(s.max, v3)
-            self.assertEqual(s.min, v1)
-            self.assertEqual(s.median, v2)
-            self.assertAlmostEqual(s.stdev, Decimal("370.83879721"))
+            assert s.sum == v1 + v2 + v3
+            assert s.avg == (v1 + v2 + v3) / Decimal(3)
+            assert s.max == v3
+            assert s.min == v1
+            assert s.median == v2
+            assert pytest.approx(s.stdev, abs=1e-8) == Decimal("370.83879721")
 
     def test_nan(self):
         nan = Decimal("nan")
         v = Decimal("0.123")
 
         s = StatValue.from_list([nan])
-        self.assertTrue(s.sum.is_nan(), f"Not NaN, but {s.sum}")
-        self.assertTrue(s.avg.is_nan(), f"Not NaN, but {s.avg}")
-        self.assertTrue(s.max.is_nan(), f"Not NaN, but {s.max}")
-        self.assertTrue(s.min.is_nan(), f"Not NaN, but {s.min}")
-        self.assertTrue(s.median.is_nan(), f"Not NaN, but {s.median}")
-        self.assertTrue(s.stdev.is_nan(), f"Not NaN, but {s.stdev}")
+        assert s.sum.is_nan()
+        assert s.avg.is_nan()
+        assert s.max.is_nan()
+        assert s.min.is_nan()
+        assert s.median.is_nan()
+        assert s.stdev.is_nan()
 
         s = StatValue.from_list([nan, v])
-        self.assertTrue(s.sum.is_nan(), f"Not NaN, but {s.sum}")
-        self.assertTrue(s.avg.is_nan(), f"Not NaN, but {s.avg}")
-        self.assertTrue(s.max.is_nan(), f"Not NaN, but {s.max}")
-        self.assertTrue(s.min.is_nan(), f"Not NaN, but {s.min}")
-        self.assertTrue(s.median.is_nan(), f"Not NaN, but {s.median}")
-        self.assertTrue(s.stdev.is_nan(), f"Not NaN, but {s.stdev}")
+        assert s.sum.is_nan()
+        assert s.avg.is_nan()
+        assert s.max.is_nan()
+        assert s.min.is_nan()
+        assert s.median.is_nan()
+        assert s.stdev.is_nan()
 
     def test_one_inf(self):
         inf = Decimal("inf")
         v = Decimal("0.123")
 
         s = StatValue.from_list([inf])
-        self.assertEqual(s.sum, inf, f"Not Inf, but {s.sum}")
-        self.assertEqual(s.avg, inf, f"Not Inf, but {s.avg}")
-        self.assertEqual(s.max, inf, f"Not Inf, but {s.max}")
-        self.assertEqual(s.min, inf, f"Not Inf, but {s.min}")
-        self.assertEqual(s.median, inf, f"Not Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == inf
+        assert s.avg == inf
+        assert s.max == inf
+        assert s.min == inf
+        assert s.median == inf
+        assert s.stdev == inf
 
         s = StatValue.from_list([inf, v])
-        self.assertEqual(s.sum, inf, f"Not Inf, but {s.sum}")
-        self.assertEqual(s.avg, inf, f"Not NaN, but {s.avg}")
-        self.assertEqual(s.max, inf, f"Not NaN, but {s.max}")
-        self.assertEqual(s.min, v, f"Not NaN, but {s.min}")
-        self.assertEqual(s.median, inf, f"Not NaN, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not NaN, but {s.stdev}")
+        assert s.sum == inf
+        assert s.avg == inf
+        assert s.max == inf
+        assert s.min == v
+        assert s.median == inf
+        assert s.stdev == inf
 
     def test_one_negative_inf(self):
         ninf = Decimal("-inf")
@@ -104,65 +97,61 @@ class TestStatValue(unittest.TestCase):
         v = Decimal("0.123")
 
         s = StatValue.from_list([ninf])
-        self.assertEqual(s.sum, ninf, f"Not -Inf, but {s.sum}")
-        self.assertEqual(s.avg, ninf, f"Not -Inf, but {s.avg}")
-        self.assertEqual(s.max, ninf, f"Not -Inf, but {s.max}")
-        self.assertEqual(s.min, ninf, f"Not -Inf, but {s.min}")
-        self.assertEqual(s.median, ninf, f"Not -Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == ninf
+        assert s.avg == ninf
+        assert s.max == ninf
+        assert s.min == ninf
+        assert s.median == ninf
+        assert s.stdev == inf
 
         s = StatValue.from_list([ninf, v])
-        self.assertEqual(s.sum, ninf, f"Not -Inf, but {s.sum}")
-        self.assertEqual(s.avg, ninf, f"Not -Inf, but {s.avg}")
-        self.assertEqual(s.max, v, f"Not 0.123, but {s.max}")
-        self.assertEqual(s.min, ninf, f"Not -Inf, but {s.min}")
-        self.assertEqual(s.median, ninf, f"Not -Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == ninf
+        assert s.avg == ninf
+        assert s.max == v
+        assert s.min == ninf
+        assert s.median == ninf
+        assert s.stdev == inf
 
     def test_multiple_positive_inf(self):
         inf = Decimal("inf")
         v = Decimal("0.123")
 
-        # Equal number of infs
         s = StatValue.from_list([inf, inf, v])
-        self.assertEqual(s.sum, inf, f"Not Inf, but {s.sum}")
-        self.assertEqual(s.avg, inf, f"Not Inf, but {s.avg}")
-        self.assertEqual(s.max, inf, f"Not Inf, but {s.max}")
-        self.assertEqual(s.min, v, f"Not 0.123, but {s.min}")
-        self.assertEqual(s.median, inf, f"Not Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == inf
+        assert s.avg == inf
+        assert s.max == inf
+        assert s.min == v
+        assert s.median == inf
+        assert s.stdev == inf
 
-        # Unequal number of infs
         s = StatValue.from_list([inf, inf, inf, v])
-        self.assertEqual(s.sum, inf, f"Not Inf, but {s.sum}")
-        self.assertEqual(s.avg, inf, f"Not Inf, but {s.avg}")
-        self.assertEqual(s.max, inf, f"Not Inf, but {s.max}")
-        self.assertEqual(s.min, v, f"Not 0.123, but {s.min}")
-        self.assertEqual(s.median, inf, f"Not Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == inf
+        assert s.avg == inf
+        assert s.max == inf
+        assert s.min == v
+        assert s.median == inf
+        assert s.stdev == inf
 
     def test_multiple_negative_inf(self):
         ninf = Decimal("-inf")
         inf = Decimal("inf")
         v = Decimal("0.123")
 
-        # Equal number of negative infs
         s = StatValue.from_list([ninf, ninf, v])
-        self.assertEqual(s.sum, ninf, f"Not -Inf, but {s.sum}")
-        self.assertEqual(s.avg, ninf, f"Not -Inf, but {s.avg}")
-        self.assertEqual(s.max, v, f"Not 0.123, but {s.max}")
-        self.assertEqual(s.min, ninf, f"Not -Inf, but {s.min}")
-        self.assertEqual(s.median, ninf, f"Not -Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == ninf
+        assert s.avg == ninf
+        assert s.max == v
+        assert s.min == ninf
+        assert s.median == ninf
+        assert s.stdev == inf
 
-        # Unequal number of negative infs
         s = StatValue.from_list([ninf, ninf, ninf, v])
-        self.assertEqual(s.sum, ninf, f"Not -Inf, but {s.sum}")
-        self.assertEqual(s.avg, ninf, f"Not -Inf, but {s.avg}")
-        self.assertEqual(s.max, v, f"Not 0.123, but {s.max}")
-        self.assertEqual(s.min, ninf, f"Not -Inf, but {s.min}")
-        self.assertEqual(s.median, ninf, f"Not -Inf, but {s.median}")
-        self.assertEqual(s.stdev, inf, f"Not Inf, but {s.stdev}")
+        assert s.sum == ninf
+        assert s.avg == ninf
+        assert s.max == v
+        assert s.min == ninf
+        assert s.median == ninf
+        assert s.stdev == inf
 
     def test_multiple_positive_and_negative_inf(self):
         inf = Decimal("inf")
@@ -170,9 +159,9 @@ class TestStatValue(unittest.TestCase):
         v = Decimal("0.123")
 
         s = StatValue.from_list([inf, ninf, v])
-        self.assertTrue(s.sum.is_nan(), f"Not NaN, but {s.sum}")
-        self.assertTrue(s.avg.is_nan(), f"Not NaN, but {s.avg}")
-        self.assertEqual(s.max, inf, f"Not Inf, but {s.max}")
-        self.assertEqual(s.min, ninf, f"Not -Inf, but {s.min}")
-        self.assertEqual(s.median, v, f"Not 0.123, but {s.median}")
-        self.assertTrue(s.stdev.is_nan(), f"Not NaN, but {s.stdev}")
+        assert s.sum.is_nan()
+        assert s.avg.is_nan()
+        assert s.max == inf
+        assert s.min == ninf
+        assert s.median == v
+        assert s.stdev.is_nan()
--- benchexec/tablegenerator/test_util.py
+++ benchexec/tablegenerator/test_util.py
@@ -6,55 +6,36 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from decimal import Decimal
-import sys
-import unittest
-
 from benchexec.tablegenerator import util
 
-sys.dont_write_bytecode = True  # prevent creation of .pyc files
-
-
-class TestUnit(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.longMessage = True
-        cls.maxDiff = None
-
-    def assertEqualNumberAndUnit(self, value, number, unit):
-        self.assertEqual(util.split_number_and_unit(value), (number, unit))
-        self.assertEqual(util.split_string_at_suffix(value, False), (number, unit))
-
-    def assertEqualTextAndNumber(self, value, text, number):
-        self.assertEqual(util.split_string_at_suffix(value, True), (text, number))
-
+class TestUnit:
     def test_split_number_and_unit(self):
-        self.assertEqualNumberAndUnit("", "", "")
-        self.assertEqualNumberAndUnit("1", "1", "")
-        self.assertEqualNumberAndUnit("1s", "1", "s")
-        self.assertEqualNumberAndUnit("111s", "111", "s")
-        self.assertEqualNumberAndUnit("s1", "s1", "")
-        self.assertEqualNumberAndUnit("s111", "s111", "")
-        self.assertEqualNumberAndUnit("-1s", "-1", "s")
-        self.assertEqualNumberAndUnit("1abc", "1", "abc")
-        self.assertEqualNumberAndUnit("abc", "", "abc")
-        self.assertEqualNumberAndUnit("abc1abc", "abc1", "abc")
-        self.assertEqualNumberAndUnit("abc1abc1abc", "abc1abc1", "abc")
+        assert util.split_number_and_unit("") == ("", "")
+        assert util.split_number_and_unit("1") == ("1", "")
+        assert util.split_number_and_unit("1s") == ("1", "s")
+        assert util.split_number_and_unit("111s") == ("111", "s")
+        assert util.split_number_and_unit("s1") == ("s1", "")
+        assert util.split_number_and_unit("s111") == ("s111", "")
+        assert util.split_number_and_unit("-1s") == ("-1", "s")
+        assert util.split_number_and_unit("1abc") == ("1", "abc")
+        assert util.split_number_and_unit("abc") == ("", "abc")
+        assert util.split_number_and_unit("abc1abc") == ("abc1", "abc")
+        assert util.split_number_and_unit("abc1abc1abc") == ("abc1abc1", "abc")
 
     def test_split_string_at_suffix(self):
-        self.assertEqualTextAndNumber("", "", "")
-        self.assertEqualTextAndNumber("1", "", "1")
-        self.assertEqualTextAndNumber("1s", "1s", "")
-        self.assertEqualTextAndNumber("111s", "111s", "")
-        self.assertEqualTextAndNumber("s1", "s", "1")
-        self.assertEqualTextAndNumber("s111", "s", "111")
-        self.assertEqualTextAndNumber("-1s", "-1s", "")
-        self.assertEqualTextAndNumber("abc1", "abc", "1")
-        self.assertEqualTextAndNumber("abc", "abc", "")
-        self.assertEqualTextAndNumber("abc1abc", "abc1abc", "")
-        self.assertEqualTextAndNumber("abc1abc1", "abc1abc", "1")
+        assert util.split_string_at_suffix("") == ("", "")
+        assert util.split_string_at_suffix("1") == ("", "1")
+        assert util.split_string_at_suffix("1s") == ("1s", "")
+        assert util.split_string_at_suffix("111s") == ("111s", "")
+        assert util.split_string_at_suffix("s1") == ("s", "1")
+        assert util.split_string_at_suffix("s111") == ("s", "111")
+        assert util.split_string_at_suffix("-1s") == ("-1s", "")
+        assert util.split_string_at_suffix("abc1") == ("abc", "1")
+        assert util.split_string_at_suffix("abc") == ("abc", "")
+        assert util.split_string_at_suffix("abc1abc") == ("abc1abc", "")
+        assert util.split_string_at_suffix("abc1abc1") == ("abc1abc", "1")
 
     def test_print_decimal_roundtrip(self):
-        # These values should be printed exactly as in the input (with "+" removed)
         test_values = [
             "NaN",
             "Inf",
@@ -79,25 +60,22 @@ class TestUnit(unittest.TestCase):
         ]
         for value in test_values:
             expected = value.lstrip("+")
-            self.assertEqual(expected, util.print_decimal(Decimal(value)))
+            assert expected == util.print_decimal(Decimal(value))
 
     def test_print_decimal_int(self):
-        # These values should be printed like Decimal prints them after quantizing
-        # to remove the exponent.
         test_values = ["0e0", "-0e0", "0e20", "1e0", "1e20", "0e10"]
         for value in test_values:
             value = Decimal(value)
             expected = str(value.quantize(1))
             assert "e" not in expected
-            self.assertEqual(expected, util.print_decimal(value))
+            assert expected == util.print_decimal(value)
 
     def test_print_decimal_float(self):
-        # These values should be printed like str prints floats.
         test_values = ["1e-4", "123e-4", "1234e-4", "1234e-5", "1234e-6"]
         for value in test_values:
             expected = str(float(value))
-            assert "e" not in expected, expected
-            self.assertEqual(expected, util.print_decimal(Decimal(value)))
+            assert "e" not in expected
+            assert expected == util.print_decimal(Decimal(value))
 
     def test_roman_number_conversion(self):
         test_data = {
@@ -127,13 +105,16 @@ class TestUnit(unittest.TestCase):
         }
 
         for k, v in test_data.items():
-            self.assertEqual(v, util.number_to_roman_string(k))
-            self.assertEqual(str(v), util.number_to_roman_string(k))
+            assert v == util.number_to_roman_string(k)
+            assert str(v) == util.number_to_roman_string(k)
 
-        self.assertRaises(ValueError, util.number_to_roman_string, -1)
-        self.assertRaises(ValueError, util.number_to_roman_string, 0)
+        with pytest.raises(ValueError):
+            util.number_to_roman_string(-1)
+        with pytest.raises(ValueError):
+            util.number_to_roman_string(0)
 
-        self.assertRaises(ValueError, util.number_to_roman_string, "forty-two")
+        with pytest.raises(ValueError):
+            util.number_to_roman_string("forty-two")
 
     def test_cap_first_letter(self):
         test_data = {
@@ -144,32 +125,26 @@ class TestUnit(unittest.TestCase):
             "": "",
         }
         for k, v in test_data.items():
-            self.assertEqual(v, util.cap_first_letter(k))
+            assert v == util.cap_first_letter(k)
 
     def test_merge_lists(self):
-        l = (1, 2, 3)  # make sure merge_lists does not modify it # noqa: E741
-        self.assertListEqual(list(l), util.merge_lists([l]))
-        self.assertListEqual(list(l), util.merge_lists([l, l]))
-        self.assertListEqual(list(l), util.merge_lists([l, l, l, l, l, l, l]))
-        self.assertListEqual(list(l), util.merge_lists([[], l, l, []]))
-        self.assertListEqual(list(l), util.merge_lists([[1, 2, 3], [1, 2], [1]]))
-        self.assertListEqual(list(l), util.merge_lists([[1, 2, 3], [2, 3], [3]]))
-        self.assertListEqual(list(l), util.merge_lists([[1], [1, 2], [1, 2, 3]]))
-        self.assertListEqual(list(l), util.merge_lists([[3], [2, 3], [1, 2, 3]]))
-        self.assertListEqual(
-            [1, 2, 3, 4, 5, 6], util.merge_lists([[1, 2, 4, 6], [1, 2, 3, 4, 5]])
-        )
+        l = (1, 2, 3)
+        assert list(l) == util.merge_lists([l])
+        assert list(l) == util.merge_lists([l, l])
+        assert list(l) == util.merge_lists([l, l, l, l, l, l, l])
+        assert list(l) == util.merge_lists([[], l, l, []])
+        assert list(l) == util.merge_lists([[1, 2, 3], [1, 2], [1]])
+        assert list(l) == util.merge_lists([[1, 2, 3], [2, 3], [3]])
+        assert list(l) == util.merge_lists([[1], [1, 2], [1, 2, 3]])
+        assert list(l) == util.merge_lists([[3], [2, 3], [1, 2, 3]])
+        assert [1, 2, 3, 4, 5, 6] == util.merge_lists([[1, 2, 4, 6], [1, 2, 3, 4, 5]])
 
     def test_find_common_elements(self):
-        self.assertListEqual([], util.find_common_elements([[]]))
-        self.assertListEqual([], util.find_common_elements([[], [1, 2, 3]]))
-        self.assertListEqual([], util.find_common_elements([[], [1, 2, 3], [1, 2, 3]]))
-        self.assertListEqual([], util.find_common_elements([[1, 2, 3], [1, 2, 3], []]))
-        self.assertListEqual([], util.find_common_elements([[1], [2], [3]]))
-        self.assertListEqual([1, 2, 3], util.find_common_elements([[1, 2, 3]]))
-        self.assertListEqual(
-            [1, 2, 3], util.find_common_elements([[1, 2, 3], [1, 2, 3]])
-        )
-        self.assertListEqual(
-            [1, 2, 3], util.find_common_elements([[1, 2, 3, 4], [1, 2, 3, 5]])
-        )
+        assert [] == util.find_common_elements([[]])
+        assert [] == util.find_common_elements([[], [1, 2, 3]])
+        assert [] == util.find_common_elements([[], [1, 2, 3], [1, 2, 3]])
+        assert [] == util.find_common_elements([[1, 2, 3], [1, 2, 3], []])
+        assert [] == util.find_common_elements([[1], [2], [3]])
+        assert [1, 2, 3] == util.find_common_elements([[1, 2, 3]])
+        assert [1, 2, 3] == util.find_common_elements([[1, 2, 3], [1, 2, 3]])
+        assert [1, 2, 3] == util.find_common_elements([[1, 2, 3, 4], [1, 2, 3, 5]])
