a998c72e0ec3a99fd41aa7602bfae9c819e794f3
passengerZ <904806153@qq.com>
2023-11-01 05:40:52 -0700

    FDSE: refactor tool info module

--- benchexec/tools/fdse.py
+++ benchexec/tools/fdse.py
@@ -12,22 +12,17 @@ import benchexec.result as result
 class Tool(benchexec.tools.template.BaseTool2):
     """
     Tool info for FDSE.
-    https://github.com/zbchen/FDSE
+    https://github.com/zbchen/FDSE (now is private)
     """
 
     def executable(self, tool_locator):
         return tool_locator.find_executable("fdse")
 
-    def program_files(self, executable):
-        return self._program_files_from_executable(
-            executable, self.REQUIRED_PATHS, parent_dir=True
-        )
-
     def version(self, executable):
-        return "fdse-testcomp"
+        return self._version_from_tool(executable, "-v")
 
     def cmdline(self, executable, options, task, rlimits):
-        new_option = ["--testcomp"]
+        new_option = []
         if task.property_file:
             new_option += [f"--property-file={task.property_file}"]
         if rlimits.cputime:
@@ -40,33 +35,17 @@ class Tool(benchexec.tools.template.BaseTool2):
         return "FDSE"
 
     def determine_result(self, run):
-        """
-        Parse the output of the tool and extract the verification result.
-        This method always needs to be overridden.
-        If the tool gave a result, this method needs to return one of the
-        benchexec.result.RESULT_* strings.
-        Otherwise an arbitrary string can be returned that will be shown to the user
-        and should give some indication of the failure reason
-        (e.g., "CRASH", "OUT_OF_MEMORY", etc.).
-        """
         for line in run.output:
             if "[BUG]" in line:
-                if line.find("ASSERTION FAIL!") != -1:
+                if "ASSERTION FAIL!" in line:
                     return result.RESULT_FALSE_REACH
-                elif line.find("Index out-of-bound") != -1:
+                elif "Index out-of-bound" in line:
                     return result.RESULT_FALSE_DEREF
-                elif line.find("overflow") != -1:
+                elif "overflow" in line:
                     return result.RESULT_FALSE_OVERFLOW
                 else:
-                    return f"ERROR ({run.exit_code.value})"
+                    return result.RESULT_ERROR
             if "Done : End analysis" in line:
                 return result.RESULT_DONE
         return result.RESULT_UNKNOWN
-
-    def get_value_from_output(self, run):
-        status = result.RESULT_UNKNOWN
-
-        if run.output.any_line_contains("DONE"):
-            status = result.RESULT_DONE
-
-        return status
+        
