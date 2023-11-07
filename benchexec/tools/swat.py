3a08e558af0c521778c08e6ceabb0a72f8b5954d
felixmaechtle <32329137+felixmaechtle@users.noreply.github.com>
2023-11-07 14:22:00 +0100

    Update swat module-info

--- benchexec/tools/swat.py
+++ benchexec/tools/swat.py
@@ -15,6 +15,15 @@ class Tool(BaseTool2):
     SWAT is currently being developed by the Institute for IT Security at the University of Luebeck.
     """
 
+    REQUIRED_PATHS = [
+        "knife-fuzzer/symbolic-executor/lib/symbolic-executor.jar",
+        "knife-fuzzer/symbolic-explorer",
+        "local_z3_installation",
+        "WitnessCreator",
+        "run-swat.sh",
+        "run_swat.py"
+    ]
+
     def executable(self, tool_locator):
         return tool_locator.find_executable("run-swat.sh")
 
@@ -23,6 +32,14 @@ class Tool(BaseTool2):
 
     def project_url(self):
         return "https://www.its.uni-luebeck.de/en/institute.html"
+
+    def version(self, executable):
+        return self._version_from_tool(executable, arg="-v")
+
+    def program_files(self, executable):
+        return self._program_files_from_executable(
+            executable, self.REQUIRED_PATHS, parent_dir=False
+        )
 
     def cmdline(self, executable, options, task, rlimits):
         cmd = [executable] + options
