0d7246b5ce9f2ef7ecf8e03acfe53296dde19a59
Levente Bajczi <bajczilevi@gmail.com>
2024-02-19 18:10:28 +0100

    Updated copyright, added readme

--- contrib/slurm/__init__.py
+++ contrib/slurm/__init__.py
@@ -2,5 +2,8 @@
 # https://github.com/sosy-lab/benchexec
 #
 # SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+# SPDX-FileCopyrightText: 2024 Levente Bajczi
+# SPDX-FileCopyrightText: Critical Systems Research Group
+# SPDX-FileCopyrightText: Budapest University of Technology and Economics <https://www.ftsrg.mit.bme.hu>
 #
 # SPDX-License-Identifier: Apache-2.0
--- contrib/slurm/slurmexecutor.py
+++ contrib/slurm/slurmexecutor.py
@@ -2,6 +2,9 @@
 # https://github.com/sosy-lab/benchexec
 #
 # SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+# SPDX-FileCopyrightText: 2024 Levente Bajczi
+# SPDX-FileCopyrightText: Critical Systems Research Group
+# SPDX-FileCopyrightText: Budapest University of Technology and Economics <https://www.ftsrg.mit.bme.hu>
 #
 # SPDX-License-Identifier: Apache-2.0
 
