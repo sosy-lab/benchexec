5199cbeebeb17df638cc3608c829d1eb5f9d7d5e
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2024-06-12 14:25:03 +0530

    some more changes

--- benchexec/tablegenerator/react-table/src/App.js
+++ benchexec/tablegenerator/react-table/src/App.js
@@ -6,31 +6,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import "./App.scss";
-import React, { Component } from "react";
+import React from "react";
 import Overview from "./components/Overview";
 
 if (process.env.NODE_ENV !== "production") {
-  // load example data for development
+  // Load example data for development
   window.data = require("@data");
   window.data.version = "(development build)";
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <main>
-          <Overview
-            data={window.data}
-            renderPlotsFlexible={true}
-            onStatsReady={this.props.onStatsReady}
-          />
-          {/* imports the component Overview with all subcomponents.
+const App = (props) => {
+  return (
+    <div className="App">
+      <main>
+        <Overview
+          data={window.data}
+          renderPlotsFlexible={true}
+          onStatsReady={props.onStatsReady}
+        />
+        {/* imports the component Overview with all subcomponents.
               The renderPlotsFlexible prop should always be true in development and production, but will be set to false in the tests to make them work.*/}
-        </main>
-      </div>
-    );
-  }
-}
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- benchexec/tablegenerator/react-table/src/components/ReactTable.js
+++ benchexec/tablegenerator/react-table/src/components/ReactTable.js
@@ -15,7 +15,7 @@ import {
   useFlexLayout,
 } from "react-table";
 import { useSticky } from "react-table-sticky";
-import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import {
   createRunSetColumns,
   StandardCell,
@@ -106,7 +106,6 @@ const Table = (props) => {
   );
   const [columnsResizeValues, setColumnsResizeValues] = useState({});
   const [disableTaskText, setDisableTaskText] = useState(false);
-  const history = useHistory();
 
   /**
    * This function automatically creates additional filters for status or category filters.
@@ -663,13 +662,15 @@ const Table = (props) => {
   }, [props.filters, filteredColumnValues, gotoPage, pageIndex, pageCount]);
 
   // Update table relevant parameters after URL change
-  useEffect(() => {
-    return history.listen((location) => {
+  const location = useLocation();
+  useEffect(
+    (_location) => {
       setPageSize(getURLParameters().pageSize || initialPageSize);
       setSortBy(getSortingSettingsFromURL());
       gotoPage(getURLParameters().page - 1 || 0);
-    });
-  }, [history, gotoPage, setPageSize, setSortBy]);
+    },
+    [location, setPageSize, setSortBy, gotoPage],
+  );
 
   const renderHeaderGroup = (headerGroup) => (
     <div className="tr headergroup" {...headerGroup.getHeaderGroupProps()}>
