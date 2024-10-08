55cadec91ce6597a48686f835fe052d4db482cee
echancrure <echancrure@gmail.com>
2024-10-08 15:59:26 +0100

    Implemented requested changes from PR

--- benchexec/tools/sikraken.py
+++ benchexec/tools/sikraken.py
@@ -4,11 +4,7 @@
 # SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
 #
 # SPDX-License-Identifier: Apache-2.0
-"""
-This class serves as BenchExec tool adaptor for Sikraken for Test-Comp
-See baseclass documentation at https://github.com/sosy-lab/benchexec/blob/main/benchexec/tools/template.py
-"""
-import os
+
 from benchexec.tools.sv_benchmarks_util import get_data_model_from_task, ILP32, LP64
 import benchexec.tools.template
 import benchexec.result as result
@@ -29,25 +25,13 @@ class Tool(benchexec.tools.template.BaseTool2):
         return tool_locator.find_executable("sikraken.sh", subdir="bin")
 
     def version(self, executable):
-        return self._version_from_tool(executable, "-v")  
-    
-    """
-    #Assuming that the default working directory is the root of the project (i.e. sikraken/) we don't need this
-    def working_directory(self, executable):
-        executableDir = os.path.dirname(executable)
-        return executableDir
-    """
-
-    #Sikraken is currently ignoring property files as we only support "coverage-branches.prp"
-    #I am assuming more options come from the benchmark definition .xml file "The benchmark definition defines the categories that the tester is to be executed on, the parameters to be passed, and the resource limits."
-    #the rlimits for testcomp are passed as options
+        return self._version_from_tool(executable, "-v")
 
     def cmdline(self, executable, options, task, rlimits):
         data_model_param = get_data_model_from_task(task, {ILP32: "-m32", LP64: "-m64"})
         options += [data_model_param]
         return [executable] + options + [task.single_input_file]
         
-    #we are always DONE
     def determine_result(self, run):
         status = result.RESULT_DONE
         return status
