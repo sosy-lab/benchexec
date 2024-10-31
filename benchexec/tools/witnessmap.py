459902e1ef9530f0b3d7f9b6a0d6999e180d94ec
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2024-10-31 15:02:13 +0100

    Add toolinfo module for WitnessMap

---
+++ benchexec/tools/witnessmap.py
@@ -0,0 +1,53 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import benchexec.result as result
+import benchexec.tools.template
+from benchexec.tools.sv_benchmarks_util import (
+    get_witness_options,
+)
+
+
+class Tool(benchexec.tools.template.BaseTool2):
+    """
+    Tool info for the witness mapper (witnessmap)
+    """
+
+    REQUIRED_PATHS = []
+
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("witness_map.py")
+
+    def name(self):
+        return "witnessmap"
+
+    def project_url(self):
+        return "https://gitlab.com/sosy-lab/software/witnessmap"
+
+    def version(self, executable):
+        version_string = self._version_from_tool(executable)
+        return version_string.partition("version")[2].strip().split(" ")[0]
+
+    def cmdline(self, executable, options, task, rlimits):
+        mapping_options = get_witness_options(options, task, ["--input"])
+
+        return [executable, *options, *mapping_options]
+
+    def get_value_from_output(self, output, identifier):
+        for line in output:
+            # Remove the log prefix containing the date, time and log level
+            line_without_log_prefix = line.split("-", maxsplit=4)[-1].strip()
+            if line_without_log_prefix.startswith(identifier):
+                return line_without_log_prefix.split(":", maxsplit=1)[-1].strip()
+        return None
+
+    def determine_result(self, run):
+        exit_code = run.exit_code.value
+        if exit_code != 0:
+            return result.RESULT_ERROR
+
+        return result.RESULT_DONE
