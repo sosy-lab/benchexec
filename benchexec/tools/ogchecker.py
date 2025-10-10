d1e6f2f2ef9562fad0152d384c4c3480305fce2a
mujtke <mujueke@mujukedeMac-mini.local>
2025-10-10 11:46:39 +0800

    Add ogchecker tool info.

---
+++ benchexec/tools/ogchecker.py
@@ -0,0 +1,21 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import benchexec.tools.cpachecker as cpachecker
+
+
+class Tool(cpachecker.Tool):
+    """Tool info for OGChecker."""
+
+    REQUIRED_PATHS = list(cpachecker.Tool.REQUIRED_PATHS) + ["resources"]
+
+    def version(self, executable):
+        return ""
+
+    def name(self):
+        return "OGChecker"
+
