5e465f843485a31114d5dd11d47a036cec41632d
Giacomo Zanatta <giacomo.zanatta@unive.it>
2025-10-16 22:13:52 +0200

    Add licensing information and fix input handling

--- benchexec/tools/jlisa.py
+++ benchexec/tools/jlisa.py
@@ -1,8 +1,13 @@
-import benchexec.util as util
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2025 UniVE-SSV
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import benchexec.tools.template
 import benchexec.result as result
 
-import sys
 
 class Tool(benchexec.tools.template.BaseTool2):
     """
@@ -11,7 +16,7 @@ class Tool(benchexec.tools.template.BaseTool2):
 
     def executable(self, tool_locator):
         return tool_locator.find_executable("jlisa")
-    
+
     def version(self, executable):
         return self._version_from_tool(executable, arg="version")
 
@@ -22,13 +27,13 @@ class Tool(benchexec.tools.template.BaseTool2):
         return "https://github.com/lisa-analyzer/jlisa"
 
     def cmdline(self, executable, options, task, rlimits):
-        cmd = [executable, "check", "--inputs", " ".join(task.input_files_or_empty)]
+        cmd = [executable, "check", "--inputs", " ".join(task.input_files)]
         if task.property_file:
             cmd += ["--property", task.property_file]
         return cmd
 
     def determine_result(self, run):
-        try:
+        if len(run.output) > 0:
             jlisa_output = run.output[-1]
             ### ASSERT ###
             if jlisa_output == "ASSERT HOLDS FOR SOME CASES BUT NOT FOR OTHERS":
@@ -43,7 +48,7 @@ class Tool(benchexec.tools.template.BaseTool2):
                 return result.RESULT_UNKNOWN
 
             ### RUNTIME ###
-            if jlisa_output == "RUNTIME HOLDS FOR SOME CASES BUT NOT FOR OTHERS": 
+            if jlisa_output == "RUNTIME HOLDS FOR SOME CASES BUT NOT FOR OTHERS":
                 return result.RESULT_UNKNOWN
             if jlisa_output == "RUNTIME DOES HOLD":
                 return result.RESULT_TRUE_PROP
@@ -53,8 +58,6 @@ class Tool(benchexec.tools.template.BaseTool2):
                 return result.RESULT_UNKNOWN
             if jlisa_output == "NO RUNTIME WARNING":
                 return result.RESULT_TRUE_PROP
-            
-            # UNKNOWN otherwise.
-            return result.RESULT_UNKNOWN
-        except:
-            return result.RESULT_UNKNOWN
+
+        # UNKNOWN otherwise.
+        return result.RESULT_UNKNOWN
