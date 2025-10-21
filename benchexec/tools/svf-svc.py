9ffab67b580354f22c94dc612d3af926ca373204
Lasagnenator <38394321+Lasagnenator@users.noreply.github.com>
2025-10-21 21:36:35 +1100

    Update to new SVF-SVC files.

--- benchexec/tools/svf-svc.py
+++ benchexec/tools/svf-svc.py
@@ -20,7 +20,7 @@ class Tool(benchexec.tools.template.BaseTool2):
     REQUIRED_PATHS = ["svf", "include_replace.c"]
 
     def executable(self, tool_locator):
-        return tool_locator.find_executable("svf_run.py")
+        return tool_locator.find_executable("python_svf.py")
 
     def name(self):
         return "SVF-SVC"
@@ -32,17 +32,7 @@ class Tool(benchexec.tools.template.BaseTool2):
         return self._version_from_tool(executable, "--version")
 
     def cmdline(self, executable, options, task, rlimits):
-        data_model_param = get_data_model_from_task(task, {ILP32: "32", LP64: "64"})
-        if data_model_param and data_model_param not in options:
-            options += ["--bits", data_model_param]
-
-        if task.property_file:
-            options += ["--prop", task.property_file]
-
-        if rlimits.cputime:
-            options += ["--time-limit", str(rlimits.cputime)]
-
-        return [executable] + options + [task.single_input_file]
+        return [executable] + [task.single_input_file] + [task.property_file]
 
     def determine_result(self, run):
         for line in run.output:
