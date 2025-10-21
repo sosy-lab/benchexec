16e3db3403bfd2eecf1ed3d81f3f9a1a848be717
naim-mr <naim.moussaoui-remil@ens-rennes.fr>
2025-10-21 12:51:48 +0200

    Fixes typo and erros

--- benchexec/tools/function.py
+++ benchexec/tools/function.py
@@ -7,10 +7,6 @@
 
 import benchexec.result as result
 import benchexec.tools.template
-from benchexec.tools.sv_benchmarks_util import (
-    TaskFilesConsidered,
-    handle_witness_of_task,
-)
 
 
 class Tool(benchexec.tools.template.BaseTool2):
@@ -31,24 +27,17 @@ class Tool(benchexec.tools.template.BaseTool2):
         return "https://github.com/naim-mr/function"
 
     def cmdline(self, executable, options, task, rlimits):
-        input_files, witness_options = handle_witness_of_task(
-            task,
-            options,
-            "--validate_yaml_witness",
-            TaskFilesConsidered.INPUT_FILES,
-        )
-        cmd = [executable] + input_files
-        return cmd + list(options)
+        cmd = [executable] + [" ", task.single_input_file]
+        return cmd
 
     def determine_result(self, run):
-        if run.was_timeout:
-            return result.RESULT_TIMEOUT
         if not run.output:
             return result.RESULT_ERROR
-        r = run.output[-1] or run.output[-2]  # last non-empty line
-        if "Final Analaysis Result: TRUE" in r:
+        if run.output.any_line_contains("Final Analysis Result: TRUE"):
             return result.RESULT_TRUE_PROP
-        elif "Final Analaysis Result: false(TERM)" in r:
+        elif run.output.any_line_contains("Final Analysis Result: false(TERM)"):
             return result.RESULT_FALSE_TERMINATION
-        else:
+        elif run.output.any_line_contains("Final Analysis Result: UNKNOWN"):
             return result.RESULT_UNKNOWN
+        else:
+            return result.RESULT_ERROR
