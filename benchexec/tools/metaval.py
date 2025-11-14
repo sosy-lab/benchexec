7aba1fea34e2b6e3d8cc60732a41fdc91b7288ab
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2025-11-14 12:28:50 +0100

    Add the functions necessary to handle version 2 of metaval and refactor the code such that each version has its own class

--- benchexec/tools/metaval.py
+++ benchexec/tools/metaval.py
@@ -16,6 +16,11 @@ from benchexec.tools.sv_benchmarks_util import (
     get_witness,
     get_single_non_witness_input_file,
     WITNESS_INPUT_FILE_IDENTIFIER,
+    get_data_model_from_task,
+    TaskFilesConsidered,
+    handle_witness_of_task,
+    LP64,
+    ILP32,
 )
 from benchexec.tools.template import (
     BaseTool2,
@@ -24,18 +29,7 @@ from benchexec.tools.template import (
 )
 
 
-class Tool(benchexec.tools.template.BaseTool2):
-    """
-    This is the tool info module for MetaVal.
-
-    The official repository is:
-    https://gitlab.com/sosy-lab/software/metaval
-
-    Please report any issues to our issue tracker at:
-    https://gitlab.com/sosy-lab/software/metaval/issues
-
-    """
-
+class MetavalVersionLessThanTwo:
     TOOL_TO_PATH_MAP = {
         "cpachecker-metaval": "CPAchecker-frontend",
         "cpachecker": "CPAchecker",
@@ -49,19 +43,10 @@ class Tool(benchexec.tools.template.BaseTool2):
     def __init__(self):
         self.lock = threading.Lock()
         self.wrappedTools = {}
-        self._cached_version: Optional[str] = None
 
-    def executable(self, tool_locator):
-        try:
-            return tool_locator.find_executable("metaval.sh")
-        except ToolNotFoundException as e1:
-            try:
-                return tool_locator.find_executable("metaval.py")
-            except ToolNotFoundException:
-                raise e1
-
-    def program_files_version_less_two(self, executable):
+    def program_files(self, executable):
         return [
+            executable,
             "VERSION.txt",
             "metaval.py",
             "metaval.sh",
@@ -69,25 +54,10 @@ class Tool(benchexec.tools.template.BaseTool2):
             self._resource(executable, path) for path in self.PATH_TO_TOOL_MAP.values()
         ]
 
-    def program_files(self, executable):
-        if self.is_version_less_than_two(executable):
-            return self.program_files_version_less_two(executable)
+    def _resource(self, executable, relpath):
+        return os.path.join(os.path.dirname(executable), relpath)
 
-    def name(self):
-        return "MetaVal"
-
-    def project_url(self):
-        return "https://gitlab.com/sosy-lab/software/metaval"
-
-    def is_version_less_than_two(self, executable):
-        version_string = self.version(executable)
-        major_version_match = re.match(r"(\d+)\..*", version_string)
-        if major_version_match:
-            major_version = int(major_version_match.group(1))
-            return major_version < 2
-        return False
-
-    def determine_result_version_less_two(self, run):
+    def determine_result(self, run):
         verifierDir = None
         regex = re.compile("verifier used in MetaVal is (.*)")
         for line in run.output[:20]:
@@ -109,11 +79,10 @@ class Tool(benchexec.tools.template.BaseTool2):
         )
         return tool.determine_result(run)
 
-    def determine_result(self, run):
-        if self.is_version_less_than_two(None):
-            return self.determine_result_version_less_two(run)
+    def name(self):
+        return "MetaVal"
 
-    def cmdline_version_less_two(self, executable, options, task, rlimits):
+    def cmdline(self, executable, options, task, rlimits):
         if not task.property_file:
             raise UnsupportedFeatureException(
                 f"Execution without property file is not supported by {self.name()}!"
@@ -194,12 +163,138 @@ class Tool(benchexec.tools.template.BaseTool2):
             + wrappedOptions
         )
 
+
+class MetavalVersionGreaterThanOrEqualTwo:
+    def program_files(self, executable):
+        return [executable] + BaseTool2._program_files_from_executable(
+            executable, ["lib", "src", "config"]
+        )
+
+    def cmdline(self, executable, options, task, rlimits):
+        if task.property_file:
+            options = options + ["--property", task.property_file]
+
+        data_model_param = get_data_model_from_task(
+            task, {ILP32: "ILP32", LP64: "LP64"}
+        )
+
+        if data_model_param and "--data-model" not in options:
+            options += ["--data-model", data_model_param]
+
+        input_file, witness_options = handle_witness_of_task(
+            task, options, "--witness", TaskFilesConsidered.SINGLE_INPUT_FILE
+        )
+
+        program_options = ["--program", input_file[0]]
+
+        return [executable] + options + witness_options + program_options
+
+    def determine_result(self, run):
+        separator = ":"
+        if not run.output:
+            return benchexec.result.RESULT_ERROR
+        lastline = run.output[-1]
+        if lastline.startswith("Witness is correct"):
+            return benchexec.result.RESULT_TRUE_PROP
+        elif lastline.startswith("Witness is incorrect"):
+            return benchexec.result.RESULT_FALSE_PROP
+        elif any(
+            lastline.startswith(start_identifier)
+            for start_identifier in (
+                "Witness could not be validated",
+                "There was an error validating the witness",
+            )
+        ):
+            prefix = (
+                benchexec.result.RESULT_UNKNOWN
+                if lastline.startswith("Witness could not be validated")
+                else benchexec.result.RESULT_ERROR
+            )
+            if separator in lastline:
+                return (
+                    prefix
+                    + "("
+                    + lastline.split(separator, maxsplit=1)[1].strip()
+                    + ")"
+                )
+            else:
+                return prefix
+        elif lastline.startswith("Witness file does not exist"):
+            return benchexec.result.RESULT_ERROR + "(no witness)"
+        else:
+            return benchexec.result.RESULT_ERROR
+
+    def get_value_from_output(self, output, identifier):
+        # Search the text line per line using the regex passed as identifier
+        # and return the first match found.
+        for line in output:
+            matches = re.search(identifier, line)
+            if matches:
+                return matches.group()
+        return None
+
+
+class Tool(benchexec.tools.template.BaseTool2):
+    """
+    This is the tool info module for MetaVal.
+
+    The official repository is:
+    https://gitlab.com/sosy-lab/software/metaval
+
+    Please report any issues to our issue tracker at:
+    https://gitlab.com/sosy-lab/software/metaval/issues
+
+    """
+
+    def __init__(self):
+        self._cached_version: Optional[str] = None
+        self.metaval_version_less_two_tool = MetavalVersionLessThanTwo()
+        self.metaval_version_geq_two_tool = MetavalVersionGreaterThanOrEqualTwo()
+
+    def executable(self, tool_locator):
+        try:
+            return tool_locator.find_executable("metaval.sh")
+        except ToolNotFoundException as e1:
+            try:
+                return tool_locator.find_executable("metaval.py")
+            except ToolNotFoundException:
+                raise e1
+
+    def program_files(self, executable):
+        if self.is_version_less_than_two(executable):
+            return self.metaval_version_less_two_tool.program_files(executable)
+        else:
+            return self.metaval_version_geq_two_tool.program_files(executable)
+
+    def name(self):
+        return "MetaVal"
+
+    def project_url(self):
+        return "https://gitlab.com/sosy-lab/software/metaval"
+
+    def is_version_less_than_two(self, executable):
+        version_string = self.version(executable)
+        major_version_match = re.match(r"(\d+)\..*", version_string)
+        if major_version_match:
+            major_version = int(major_version_match.group(1))
+            return major_version < 2
+        return False
+
+    def determine_result(self, run):
+        if self.is_version_less_than_two(None):
+            return self.metaval_version_less_two_tool.determine_result(run)
+        else:
+            return self.metaval_version_geq_two_tool.determine_result(run)
+
     def cmdline(self, executable, options, task, rlimits):
         if self.is_version_less_than_two(executable):
-            return self.cmdline_version_less_two(executable, options, task, rlimits)
-
-    def _resource(self, executable, relpath):
-        return os.path.join(os.path.dirname(executable), relpath)
+            return self.metaval_version_less_two_tool.cmdline(
+                executable, options, task, rlimits
+            )
+        else:
+            return self.metaval_version_geq_two_tool.cmdline(
+                executable, options, task, rlimits
+            )
 
     def version(self, executable):
         if self._cached_version is not None:
@@ -209,3 +304,12 @@ class Tool(benchexec.tools.template.BaseTool2):
         metaval_version = stdout.splitlines()[0].strip()
         self._cached_version = metaval_version
         return metaval_version
+
+    def get_value_from_output(self, output, identifier):
+        if self.is_version_less_than_two(None):
+            # Not implemented for versions less than 2
+            return None
+
+        return self.metaval_version_geq_two_tool.get_value_from_output(
+            output, identifier
+        )
