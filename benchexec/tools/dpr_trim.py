0bcf439a0ed32e3f6016bfc27c2e0c9e9b61d8a5
Henrik Wachowitz <henrik.wachowitz@ifi.lmu.de>
2026-06-04 11:49:47 -0700

    copy raw tool modules from the replication

---
+++ benchexec/tools/dpr_trim.py
@@ -0,0 +1,49 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import cast
+
+from benchexec import result
+from benchexec.tools import template
+
+
+class Tool(template.BaseTool2):
+    """
+    Tool-info module for sat solvers that were executed on the StarExec platform.
+    """
+
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("dpr-trim")
+
+    def version(self, executable):
+        return "TODO"
+
+    def name(self):
+        return "DPR-Trim"
+
+    def cmdline(
+        self, executable, options: list[str], task: template.BaseTool2.Task, rlimits
+    ):
+        """ The proof must be the first argument in the options list. """
+        
+        return [executable, task.single_input_file, *options]
+
+    def determine_result(self, run: template.BaseTool2.Run):
+        output = cast(template.BaseTool2.RunOutput, run.output)
+        for line in output:
+            line = cast(str, line)
+            if line.startswith("s "):
+                verdict = line.strip().split(" ")[1].strip().upper()
+                try:
+                    sat_arg = f"({line.strip().split(' ')[2].strip().upper()})"
+                except IndexError:
+                    sat_arg = ""
+                if verdict == "VERIFIED":
+                    return result.RESULT_TRUE_PROP + sat_arg
+                elif verdict == "ERROR":
+                    return result.RESULT_ERROR
+        return result.RESULT_ERROR
---
+++ benchexec/tools/hcad.py
@@ -0,0 +1,58 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from benchexec.tools import template
+
+from benchexec import result
+
+
+class Tool(template.BaseTool2):
+    """
+    Tool-info module for sat solvers that were executed on the StarExec platform.
+    """
+
+    REQUIRED_PATHS = ["bin"]
+
+    def executable(self, tool_locator):
+        return tool_locator.find_executable("starexec_run_pbva", subdir="bin")
+
+    def version(self, executable):
+        return "TODO"
+
+    def program_files(self, executable):
+        return self._program_files_from_executable(
+            executable, self.REQUIRED_PATHS, parent_dir=True
+        )
+
+    def name(self):
+        return "SAT Solver"
+
+    def working_directory(self, executable):
+        from pathlib import Path
+
+        return str(Path(executable).parent)
+
+    def cmdline(self, executable, options, task, rlimits):
+        if "pbva" in options:
+            options.remove("pbva")
+        elif "bva" in options:
+            options.remove("bva")
+            executable = executable.replace("starexec_run_pbva", "starexec_run_bva")
+
+        return [executable, task.single_input_file, *options]
+
+    def determine_result(self, run):
+        for line in reversed(run.output):
+            if line.startswith("s "):
+                verdict = line.strip().partition(" ")[-1].strip().upper()
+                if verdict.startswith("SATISFIABLE"):
+                    return result.RESULT_TRUE_PROP
+                elif verdict.startswith("UNSATISFIABLE"):
+                    return result.RESULT_FALSE_PROP
+                elif verdict.startswith("UNKNOWN"):
+                    return result.RESULT_UNKNOWN
+        return result.RESULT_ERROR
