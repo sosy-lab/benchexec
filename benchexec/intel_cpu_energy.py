59fd3e7c340d935f42d7b0a90ec58fea2302a678
Nick <nicolas.schlueter@campus.lmu.de>
2026-08-04 19:01:07 +0200

    added immutability for Package and Domain

--- benchexec/intel_cpu_energy.py
+++ benchexec/intel_cpu_energy.py
@@ -18,17 +18,22 @@ rapl_path = Path("/sys/class/powercap/intel-rapl/")
 
 
 @dataclass
+class EnergyWrapper:  # This wrapper is needed to keep immutability in the Domain and Package classes while still allowing energy values to be updated
+    value: int
+
+
+@dataclass(frozen=True)
 class Domain:
     name: str
     path: Path
-    energy: int
+    energy: EnergyWrapper
 
 
-@dataclass
+@dataclass(frozen=True)
 class Package:
     name: str
     path: Path
-    energy: int
+    energy: EnergyWrapper
     domains: list[Domain]
 
 
@@ -45,9 +50,9 @@ class EnergyMeasurement(object):
                 d for d in package.glob("intel-rapl:*") if d.name.count(":") == 2
             ):
                 d_name = (domain / "name").read_text().strip()
-                domains.append(Domain(d_name, domain, 0))
+                domains.append(Domain(d_name, domain, EnergyWrapper(0)))
 
-            self.packages.append(Package(p_name, package, 0, domains))
+            self.packages.append(Package(p_name, package, EnergyWrapper(0), domains))
 
     @classmethod
     def create_if_supported(cls):
@@ -76,9 +81,9 @@ class EnergyMeasurement(object):
     def __str__(self):
         string = ""
         for package in self.packages:
-            string += f"{package.name}: {package.energy} uj\n"
+            string += f"{package.name}: {package.energy.value} uj\n"
             for domain in package.domains:
-                string += f"    {domain.name}: {domain.energy} uj\n"
+                string += f"    {domain.name}: {domain.energy.value} uj\n"
         return string
 
 
@@ -87,19 +92,19 @@ def update_values(measurement):
     we can use the same code because the energy values are initialized with 0"""
     for package in measurement.packages:
         new_energy = int((package.path / "energy_uj").read_text().strip())
-        if new_energy < package.energy:
+        if new_energy < package.energy.value:
             new_energy += int(
                 (package.path / "max_energy_range_uj").read_text().strip()
             )
-        package.energy = new_energy - package.energy
+        package.energy.value = new_energy - package.energy.value
 
         for domain in package.domains:
             new_energy = int((domain.path / "energy_uj").read_text().strip())
-            if new_energy < domain.energy:
+            if new_energy < domain.energy.value:
                 new_energy += int(
                     (domain.path / "max_energy_range_uj").read_text().strip()
                 )
-            domain.energy = new_energy - domain.energy
+            domain.energy.value = new_energy - domain.energy.value
 
 
 def convert_to_joules(energy):
@@ -115,16 +120,15 @@ def format_energy_results(measurement):
     result = {}
     total = Decimal(0)
     for package in measurement.packages:
-        p_energy = convert_to_joules(package.energy)
-        if (
-            not package.name == "psys"
-        ):  # psys describes energy usage of the entire system and is therefore not relevant for us
+        p_energy = convert_to_joules(package.energy.value)
+        # psys describes energy usage of the entire system and is therefore not relevant for us
+        if not package.name == "psys":
             total += p_energy
             result[f"cpuenergy-{package.name}"] = p_energy
         else:
             result["psys"] = p_energy
         for domain in package.domains:
-            d_energy = convert_to_joules(domain.energy)
+            d_energy = convert_to_joules(domain.energy.value)
             result[f"cpuenergy-{package.name}-{domain.name}"] = d_energy
     result["cpuenergy"] = total
     result = collections.OrderedDict(sorted(result.items()))
